3400c372fe565a4185c3a8a346591abea5b151c7
Josh Walawender <jmwalawender@gmail.com>
2013-08-05 16:39:28 -1000

    Removed FanControl.py as it had no useful content. Removed Focus.py as it is for the old IQMon and is archived in that repo. Renamed files to remove VYSOS_ from filename as it is no longer needed now that they are in a separate directory from IQMon. Replaced all tabs with spaces for consistency. Edits to several files to make compatible with new IQMon (work in ongoing, unfinished).

--- MeasureImage.py
+++ MeasureImage.py
@@ -25,10 +25,10 @@ import IQMon
 
 
 class ParseError(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return repr(self.value)
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 
 ##############################################################
@@ -42,205 +42,207 @@ class ParseError(Exception):
 ## - write that combined dark to a file named for the DataNight (not the night it was taken)
 ##############################################################
 def ListDarks(image, tel, config, logger):
-	nDarksMin = 5  ## Minimum number of dark files to return for combination
-	SearchNDays = 10 ## Number of days back in time to look for dark frames
-	logger.info("Looking for master dark frame or darks to combine.")
-	## Extract night data was taken from path
-	DataPath = os.path.split(image.rawFile)[0]
-	BaseDirectory, DataNightString = os.path.split(DataPath)
-	DataNight = datetime.datetime.strptime(DataNightString, "%Y%m%dUT")
+    nDarksMin = 5  ## Minimum number of dark files to return for combination
+    SearchNDays = 10 ## Number of days back in time to look for dark frames
+    logger.info("Looking for master dark frame or darks to combine.")
+    ## Extract night data was taken from path
+    DataPath = os.path.split(image.rawFile)[0]
+    BaseDirectory, DataNightString = os.path.split(DataPath)
+    DataNight = datetime.datetime.strptime(DataNightString, "%Y%m%dUT")
 
-	OneDay = datetime.timedelta(days=1)
-	NewDate = DataNight
-	NewDateString = datetime.datetime.strftime(NewDate, "%Y%m%dUT")
-	DateLimit = DataNight - datetime.timedelta(days=SearchNDays)
-	
-	## Check to see if MasterDark Exists for this Observation Date
-	MasterDarkFilename = "MasterDark_"+tel.name+"_"+DataNightString+"_"+str(int(math.floor(image.exptime.to(u.s).value)))+".fits"
-	MasterDarkFile  = os.path.join(config.pathTemp, MasterDarkFilename)	
-	## Is that Master Dark File does not exist, see if the raw files exit to build one.
-	if os.path.exists(MasterDarkFile):
-		logger.info("Found Master Dark: %s" % MasterDarkFilename)
-		return [MasterDarkFile]
-	else:
-		logger.info("Could Not Find Master Dark.  Looking for raw frames.")
-		Darks = []
-		while NewDate > DateLimit:
-			## Look for this directory
-			SearchPath = os.path.join(BaseDirectory, NewDateString, "Calibration")
-			if os.path.exists(SearchPath):
-				## Now look for darks in that directory
-				logger.debug("Looking for darks in {0}".format(SearchPath))
-				Files = os.listdir(SearchPath)
-				for File in Files:
-					IsDark = re.match("Dark\-([0-9]{3})\-([0-9]{8})at([0-9]{6})\.fi?ts", File)
-					if IsDark:
-						DarkExp = float(IsDark.group(1))
-						if DarkExp == image.exptime.value:
-							Darks.append(os.path.join(SearchPath, File.replace("fts", "fits")))
-				if len(Darks) >= nDarksMin:
-					## Once we have found enough dark files, return the list of dark files
-					logger.info("Found %d dark files in %s" % (len(Darks), SearchPath))
-					for Dark in Darks:
-						logger.debug("Found Dark File: {0}".format(Dark))
-					return Darks
-			NewDate = NewDate - OneDay
-			NewDateString = datetime.datetime.strftime(NewDate, "%Y%m%dUT")
-		if len(Darks) == 0:
-			logger.warning("No darks found to combine.")
+    OneDay = datetime.timedelta(days=1)
+    NewDate = DataNight
+    NewDateString = datetime.datetime.strftime(NewDate, "%Y%m%dUT")
+    DateLimit = DataNight - datetime.timedelta(days=SearchNDays)
+    
+    ## Check to see if MasterDark Exists for this Observation Date
+    MasterDarkFilename = "MasterDark_"+tel.name+"_"+DataNightString+"_"+str(int(math.floor(image.exptime.to(u.s).value)))+".fits"
+    MasterDarkFile  = os.path.join(config.pathTemp, MasterDarkFilename)    
+    ## Is that Master Dark File does not exist, see if the raw files exit to build one.
+    if os.path.exists(MasterDarkFile):
+        logger.info("Found Master Dark: %s" % MasterDarkFilename)
+        return [MasterDarkFile]
+    else:
+        logger.info("Could Not Find Master Dark.  Looking for raw frames.")
+        Darks = []
+        while NewDate > DateLimit:
+            ## Look for this directory
+            SearchPath = os.path.join(BaseDirectory, NewDateString, "Calibration")
+            if os.path.exists(SearchPath):
+                ## Now look for darks in that directory
+                logger.debug("Looking for darks in {0}".format(SearchPath))
+                Files = os.listdir(SearchPath)
+                for File in Files:
+                    IsDark = re.match("Dark\-([0-9]{3})\-([0-9]{8})at([0-9]{6})\.fi?ts", File)
+                    if IsDark:
+                        DarkExp = float(IsDark.group(1))
+                        if DarkExp == image.exptime.value:
+                            Darks.append(os.path.join(SearchPath, File.replace("fts", "fits")))
+                if len(Darks) >= nDarksMin:
+                    ## Once we have found enough dark files, return the list of dark files
+                    logger.info("Found %d dark files in %s" % (len(Darks), SearchPath))
+                    for Dark in Darks:
+                        logger.debug("Found Dark File: {0}".format(Dark))
+                    return Darks
+            NewDate = NewDate - OneDay
+            NewDateString = datetime.datetime.strftime(NewDate, "%Y%m%dUT")
+        if len(Darks) == 0:
+            logger.warning("No darks found to combine.")
 
 
 ##-------------------------------------------------------------------------
 ## Main Program
 ##-------------------------------------------------------------------------
 def main():
-	##-------------------------------------------------------------------------
-	## Parse Command Line Arguments
-	##-------------------------------------------------------------------------
-	## create a parser object for understanding command-line arguments
-	parser = ArgumentParser(description="Describe the script")
-	## add flags
-	parser.add_argument("-v", "--verbose",
+    ##-------------------------------------------------------------------------
+    ## Parse Command Line Arguments
+    ##-------------------------------------------------------------------------
+    ## create a parser object for understanding command-line arguments
+    parser = ArgumentParser(description="Describe the script")
+    ## add flags
+    parser.add_argument("-v", "--verbose",
         action="store_true", dest="verbose",
-		default=False, help="Be verbose! (default = False)")
-	parser.add_argument("-c", "--clobber",
+        default=False, help="Be verbose! (default = False)")
+    parser.add_argument("-c", "--clobber",
         action="store_true", dest="clobber",
-		default=False, help="Delete previous logs and summary files for this image. (default = False)")
-	## add arguments
-	parser.add_argument("filename",
-	    type=str,
-		help="File Name of Input Image File")
-	parser.add_argument("-t", dest="telescope",
-	    required=False, type=str,
-		help="Telescope which tool the data ('V5' or 'V20')")
-	args = parser.parse_args()
-	
-	##-------------------------------------------------------------------------
-	## Deconstruct input filename in to path, filename and extension
-	##-------------------------------------------------------------------------
-	FitsFile = os.path.abspath(args.filename)
-	if not os.path.exists(FitsFile):
-		raise IOError("Unable to find input file: %s" % FitsFile)
-	FitsFileDirectory, FitsFilename = os.path.split(FitsFile)
-	DataNightString = os.path.split(FitsFileDirectory)[1]
-	FitsBasename, FitsExt = os.path.splitext(FitsFilename)
+        default=False, help="Delete previous logs and summary files for this image. (default = False)")
+    ## add arguments
+    parser.add_argument("filename",
+        type=str,
+        help="File Name of Input Image File")
+    parser.add_argument("-t", dest="telescope",
+        required=False, type=str,
+        help="Telescope which tool the data ('V5' or 'V20')")
+    args = parser.parse_args()
+    
+    ##-------------------------------------------------------------------------
+    ## Deconstruct input filename in to path, filename and extension
+    ##-------------------------------------------------------------------------
+    FitsFile = os.path.abspath(args.filename)
+    if not os.path.exists(FitsFile):
+        raise IOError("Unable to find input file: %s" % FitsFile)
+    FitsFileDirectory, FitsFilename = os.path.split(FitsFile)
+    DataNightString = os.path.split(FitsFileDirectory)[1]
+    FitsBasename, FitsExt = os.path.splitext(FitsFilename)
 
 
-	##-------------------------------------------------------------------------
-	## Establish IQMon Configuration
-	##-------------------------------------------------------------------------
-	config = IQMon.Config()
+    ##-------------------------------------------------------------------------
+    ## Establish IQMon Configuration
+    ##-------------------------------------------------------------------------
+    config = IQMon.Config()
 
 
-	##-------------------------------------------------------------------------
-	## Determine which VYSOS Telescope Image is from
-	##-------------------------------------------------------------------------
-	if args.telescope == 'V5' or args.telescope == 'V20':
-		telescope = args.telescope
-	else:
-		V5match = re.match("V5.*\.fi?ts", FitsFilename)
-		V20match = re.match("V20.*\.fi?ts", FitsFilename)
-		if V5match and not V20match:
-			telescope = "V5"
-		elif V20match and not V5match:
-			telescope = "V20"
-		else:
-			raise ParseError("Can not determine valid telescope from arguments or filename.")
+    ##-------------------------------------------------------------------------
+    ## Determine which VYSOS Telescope Image is from
+    ##-------------------------------------------------------------------------
+    if args.telescope == 'V5' or args.telescope == 'V20':
+        telescope = args.telescope
+    else:
+        V5match = re.match("V5.*\.fi?ts", FitsFilename)
+        V20match = re.match("V20.*\.fi?ts", FitsFilename)
+        if V5match and not V20match:
+            telescope = "V5"
+        elif V20match and not V5match:
+            telescope = "V20"
+        else:
+            raise ParseError("Can not determine valid telescope from arguments or filename.")
 
 
-	##-------------------------------------------------------------------------
-	## Create Telescope Object
-	##-------------------------------------------------------------------------
-	tel = IQMon.Telescope()
-	tel.name = telescope
-	if tel.name == "V5":
-		tel.longName = "VYSOS-5"
-		tel.focalLength = 735.*u.mm
-		tel.pixelSize = 9.0*u.micron
-		tel.aperture = 135.*u.mm
-		tel.gain = 1.6 / u.adu
-		tel.unitsForFWHM = 1.*u.pix
-		tel.ROI = "[1024:3072,1024:3072]"
-		tel.thresholdFWHM = 2.5*u.pix
-		tel.thresholdPointingErr = 5.0*u.arcmin
-		tel.thresholdEllipticity = 0.30*u.dimensionless_unscaled
-		tel.pixelScale = tel.pixelSize.to(u.mm)/tel.focalLength.to(u.mm)*u.radian.to(u.arcsec)
-		tel.fRatio = tel.focalLength.to(u.mm)/tel.aperture.to(u.mm)
-		tel.SExtractorPhotAperture = 6.0*u.pix
-		tel.SExtractorSeeing = 2.0*u.arcsec
-	if tel.name == "V20":
-		tel.longName = "VYSOS-20"
-		tel.focalLength = 4175.*u.mm
-		tel.pixelSize = 9.0*u.micron
-		tel.aperture = 508.*u.mm
-		tel.gain = 1.6 / u.adu
-		tel.unitsForFWHM = 1.*u.arcsec
-		tel.ROI = "[1024:3072,1024:3072]"
-		tel.thresholdFWHM = 2.5*u.arcsec
-		tel.thresholdPointingErr = 5.0*u.arcmin
-		tel.thresholdEllipticity = 0.30*u.dimensionless_unscaled
-		tel.pixelScale = tel.pixelSize.to(u.mm)/tel.focalLength.to(u.mm)*u.radian.to(u.arcsec)
-		tel.fRatio = tel.focalLength.to(u.mm)/tel.aperture.to(u.mm)
-		tel.SExtractorPhotAperture = 16.0*u.pix
-		tel.SExtractorSeeing = 2.0*u.arcsec
-	## Define Site (ephem site object)
-	tel.site = ephem.Observer()
+    ##-------------------------------------------------------------------------
+    ## Create Telescope Object
+    ##-------------------------------------------------------------------------
+    tel = IQMon.Telescope()
+    tel.name = telescope
+    if tel.name == "V5":
+        tel.longName = "VYSOS-5"
+        tel.focalLength = 735.*u.mm
+        tel.pixelSize = 9.0*u.micron
+        tel.aperture = 135.*u.mm
+        tel.gain = 1.6 / u.adu
+        tel.unitsForFWHM = 1.*u.pix
+        tel.ROI = "[1024:3072,1024:3072]"
+        tel.thresholdFWHM = 2.5*u.pix
+        tel.thresholdPointingErr = 5.0*u.arcmin
+        tel.thresholdEllipticity = 0.30*u.dimensionless_unscaled
+        tel.pixelScale = tel.pixelSize.to(u.mm)/tel.focalLength.to(u.mm)*u.radian.to(u.arcsec)
+        tel.fRatio = tel.focalLength.to(u.mm)/tel.aperture.to(u.mm)
+        tel.SExtractorPhotAperture = 6.0*u.pix
+        tel.SExtractorSeeing = 2.0*u.arcsec
+    if tel.name == "V20":
+        tel.longName = "VYSOS-20"
+        tel.focalLength = 4175.*u.mm
+        tel.pixelSize = 9.0*u.micron
+        tel.aperture = 508.*u.mm
+        tel.gain = 1.6 / u.adu
+        tel.unitsForFWHM = 1.*u.arcsec
+        tel.ROI = "[1024:3072,1024:3072]"
+        tel.thresholdFWHM = 2.5*u.arcsec
+        tel.thresholdPointingErr = 5.0*u.arcmin
+        tel.thresholdEllipticity = 0.30*u.dimensionless_unscaled
+        tel.pixelScale = tel.pixelSize.to(u.mm)/tel.focalLength.to(u.mm)*u.radian.to(u.arcsec)
+        tel.fRatio = tel.focalLength.to(u.mm)/tel.aperture.to(u.mm)
+        tel.SExtractorPhotAperture = 16.0*u.pix
+        tel.SExtractorSeeing = 2.0*u.arcsec
+    ## Define Site (ephem site object)
+    tel.site = ephem.Observer()
 
 
-	##-------------------------------------------------------------------------
-	## Create IQMon.Image Object
-	##-------------------------------------------------------------------------
-	image = IQMon.Image(FitsFile)  ## Create image object
+    ##-------------------------------------------------------------------------
+    ## Create IQMon.Image Object
+    ##-------------------------------------------------------------------------
+    image = IQMon.Image(FitsFile)  ## Create image object
 
-	##-------------------------------------------------------------------------
-	## Create Logger Object
-	##-------------------------------------------------------------------------
-	IQMonLogFileName = os.path.join(config.pathLog, tel.longName, image.rawFileBasename+"_"+tel.name+"_IQMonLog.txt")
-	if clobber:
-		if os.path.exists(IQMonLogFileName): os.remove(IQMonLogFileName)
-	logger = logging.getLogger('IQMonLogger')
-	logger.setLevel(logging.DEBUG)
-	LogFileHandler = logging.FileHandler(IQMonLogFileName)
-	LogFileHandler.setLevel(logging.DEBUG)
-	LogConsoleHandler = logging.StreamHandler()
-	if args.verbose:
-		LogConsoleHandler.setLevel(logging.DEBUG)
-	else:
-		LogConsoleHandler.setLevel(logging.INFO)
-	LogFormat = logging.Formatter('%(asctime)23s %(levelname)8s: %(message)s')
-	LogFileHandler.setFormatter(LogFormat)
-	LogConsoleHandler.setFormatter(LogFormat)
-	logger.addHandler(LogConsoleHandler)
-	logger.addHandler(LogFileHandler)
+    ##-------------------------------------------------------------------------
+    ## Create Logger Object
+    ##-------------------------------------------------------------------------
+    IQMonLogFileName = os.path.join(config.pathLog, tel.longName, image.rawFileBasename+"_"+tel.name+"_IQMonLog.txt")
+    if args.clobber:
+        if os.path.exists(IQMonLogFileName): os.remove(IQMonLogFileName)
+    logger = logging.getLogger('IQMonLogger')
+    logger.setLevel(logging.DEBUG)
+    LogFileHandler = logging.FileHandler(IQMonLogFileName)
+    LogFileHandler.setLevel(logging.DEBUG)
+    LogConsoleHandler = logging.StreamHandler()
+    if args.verbose:
+        LogConsoleHandler.setLevel(logging.DEBUG)
+    else:
+        LogConsoleHandler.setLevel(logging.INFO)
+    LogFormat = logging.Formatter('%(asctime)23s %(levelname)8s: %(message)s')
+    LogFileHandler.setFormatter(LogFormat)
+    LogConsoleHandler.setFormatter(LogFormat)
+    logger.addHandler(LogConsoleHandler)
+    logger.addHandler(LogFileHandler)
 
 
-	##-------------------------------------------------------------------------
-	## Perform Actual Image Analysis
-	##-------------------------------------------------------------------------
-	logger.info("###### Processing Image:  %s ######", FitsFilename)
-	logger.info("Setting telescope variable to %s", telescope)
-	tel.CheckUnits(logger)
-	image.htmlImageList = os.path.join(config.pathLog, tel.longName, DataNightString+"_"+tel.name+".html")
-	if clobber:
-		if os.path.exists(image.htmlImageList): os.remove(image.htmlImageList)
-	image.ReadImage(config)        ## Create working copy of image (don't edit raw file!)
-	image.GetHeader(tel, logger)   ## Extract values from header
-	image.MakeJPEG(image.rawFileBasename+"_full.jpg", tel, config, logger, rotate=True, binning=2)
-	if not image.imageWCS:
-		image.SolveAstrometry(tel, config, logger)  ## Solve Astrometry
-		image.GetHeader(logger)                     ## Refresh Header
-	image.DeterminePointingError(logger)            ## Calculate Pointing Error
-	darks = ListDarks(image, tel, config, logger)   ## List dark files
-	image.DarkSubtract(darks, tel, config, logger)  ## Dark Subtract Image
-	image.Crop(tel, logger)                         ## Crop Image
-	image.GetHeader(tel, logger)                    ## Refresh Header
-	image.RunSExtractor(tel, config, logger)        ## Run SExtractor
-	image.DetermineFWHM(logger)
-	image.MakeJPEG(image.rawFileBasename+"_crop.jpg", tel, config, logger, marked=True, binning=1)
-	image.CleanUp(logger)
-	image.CalculateProcessTime(logger)
-	image.AddWebLogEntry(tel, config, logger)
-	
+    ##-------------------------------------------------------------------------
+    ## Perform Actual Image Analysis
+    ##-------------------------------------------------------------------------
+    logger.info("###### Processing Image:  %s ######", FitsFilename)
+    logger.info("Setting telescope variable to %s", telescope)
+    tel.CheckUnits(logger)
+    image.htmlImageList = os.path.join(config.pathLog, tel.longName, DataNightString+"_"+tel.name+".html")
+    image.summaryFile = os.path.join(config.pathLog, tel.longName, DataNightString+"_"+tel.name+"_Summary.txt")
+    if args.clobber:
+        if os.path.exists(image.htmlImageList): os.remove(image.htmlImageList)
+    image.ReadImage(config)        ## Create working copy of image (don't edit raw file!)
+    image.GetHeader(tel, logger)   ## Extract values from header
+#     image.MakeJPEG(image.rawFileBasename+"_full.jpg", tel, config, logger, rotate=True, binning=2)
+    if not image.imageWCS:
+        image.SolveAstrometry(tel, config, logger)  ## Solve Astrometry
+        image.GetHeader(logger)                     ## Refresh Header
+    image.DeterminePointingError(logger)            ## Calculate Pointing Error
+    darks = ListDarks(image, tel, config, logger)   ## List dark files
+    image.DarkSubtract(darks, tel, config, logger)  ## Dark Subtract Image
+    image.Crop(tel, logger)                         ## Crop Image
+    image.GetHeader(tel, logger)                    ## Refresh Header
+    image.RunSExtractor(tel, config, logger)        ## Run SExtractor
+    image.DetermineFWHM(logger)
+#     image.MakeJPEG(image.rawFileBasename+"_crop.jpg", tel, config, logger, marked=True, binning=1)
+    image.CleanUp(logger)
+    image.CalculateProcessTime(logger)
+    image.AddWebLogEntry(tel, config, logger)
+    image.AddSummaryEntry(logger)
+    
 
 if __name__ == '__main__':
-	main()
+    main()
--- fts2fits.py
+++ fts2fits.py
@@ -19,40 +19,40 @@ The help message goes here.
 
 
 class Usage(Exception):
-	def __init__(self, msg):
-		self.msg = msg
+    def __init__(self, msg):
+        self.msg = msg
 
 
 def main(argv=None):
-	if argv is None:
-		argv = sys.argv
-	try:
-		try:
-			opts, args = getopt.getopt(argv[1:], "hv", ["help"])
-		except getopt.error, msg:
-			raise Usage(msg)
-	
-		# option processing
-		for option, value in opts:
-			if option == "-v":
-				verbose = True
-			if option in ("-h", "--help"):
-				raise Usage(help_message)
-	
-	except Usage, err:
-		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-		print >> sys.stderr, "\t for help use --help"
-		return 2
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hv", ["help"])
+        except getopt.error, msg:
+            raise Usage(msg)
+    
+        # option processing
+        for option, value in opts:
+            if option == "-v":
+                verbose = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+    
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
 
 
-	for item in sys.argv[1:]:
-		if fnmatch.fnmatch(item, "*.fts"):
-			newitem = item.replace("fts", "fits")
-			print "copying %s to %s" % (item, newitem)
-			if not os.path.exists(newitem):
-				shutil.copy(item, newitem)
-			else:
-				print "  %s already exists" % newitem
+    for item in sys.argv[1:]:
+        if fnmatch.fnmatch(item, "*.fts"):
+            newitem = item.replace("fts", "fits")
+            print "copying %s to %s" % (item, newitem)
+            if not os.path.exists(newitem):
+                shutil.copy(item, newitem)
+            else:
+                print "  %s already exists" % newitem
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
