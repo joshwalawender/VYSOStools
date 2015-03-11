03e8ff7c1c1f27c7d9f48b4dcd3eac391ee445b1
Josh Walawender <jmwalawender@gmail.com>
2015-03-11 09:46:25 -1000

    Reformat web page and put Boltwood and V20 status up.

--- WebSite/web_server.py
+++ WebSite/web_server.py
@@ -21,17 +21,28 @@ class HelloHandler(RequestHandler):
 
 class StatusHandler(RequestHandler):
     def get(self):
-        now = datetime.datetime.utcnow()
-        UTdate_string = now.strftime('%Y%m%dUT')
+        now = datetime.datetime.now()
+        nowut = datetime.datetime.utcnow()
 
         client = MongoClient('192.168.1.101', 27017)
         v20status = client.vysos['v20status']
 
         entries = [entry for entry\
-                   in v20status.find( {"UT date" : UTdate_string} ).sort([('UT time', pymongo.ASCENDING)])]
-        print()
+                   in v20status.find( {"UT date" : now.strftime('%Y%m%dUT')} ).sort([('UT time', pymongo.ASCENDING)])]
 
-        self.render("status.html", title="VYSOS Status", data = entries[-1])
+        clarity_time = datetime.datetime.strptime('{} {}'.format(entries[-1]['boltwood date'], entries[-1]['boltwood time'][:-3]), '%Y-%m-%d %H:%M:%S')
+        clarity_age = (now - clarity_time).total_seconds()
+
+        data_time = datetime.datetime.strptime('{} {}'.format(entries[-1]['UT date'], entries[-1]['UT time']), '%Y%m%dUT %H:%M:%S')
+        data_age = (nowut - data_time).total_seconds()
+
+        self.render("status.html", title="VYSOS Status",\
+                    date = nowut.strftime('%Y%m%dUT'),\
+                    time = nowut.strftime('%H:%M:%S'),\
+                    clarity_age = clarity_age,\
+                    data_age = data_age,\
+                    data = entries[-1],\
+                    )
 
 def make_app():
     return Application([
