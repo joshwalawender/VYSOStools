d8f83764db81a93a1dc442adf825950d54eef596
Josh Walawender <jmwalawender@gmail.com>
2015-03-11 10:16:08 -1000

    Replace status and condition inregers with strings

--- WebSite/web_server.py
+++ WebSite/web_server.py
@@ -29,11 +29,38 @@ class StatusHandler(RequestHandler):
 
         entries = [entry for entry\
                    in v20status.find( {"UT date" : now.strftime('%Y%m%dUT')} ).sort([('UT time', pymongo.ASCENDING)])]
+        data = entries[-1]
+        
+        wind_units = {'M': 'mph', 'K': 'kph', 'm': 'm/s'}
+        rain_status = {0: 'Dry', 1: 'Recent Rain', 2: 'Raining'}
+        wet_status = {0: 'Dry', 1: 'Recent Wet', 2: 'Wet'}
+        cloud_condition = {0: 'Unknown', 1: 'Clear', 2: 'Cloudy', 3: 'Very Cloudy'}
+        wind_condition = {0: 'Unknown', 1: 'Calm', 2: 'Windy', 3: 'Very Windy'}
+        rain_condition = {0: 'Unknown', 1: 'Dry', 2: 'Wet', 3: 'Rain'}
+        day_condition = {0: 'Unknown', 1: 'Dark', 2: 'Light', 3: 'Very Light'}
+        roof_close = {0: 'Safe', 1: 'Unsafe'}
+
+        if 'boltwood wind units' in data.keys():
+            data['boltwood wind units'] = wind_units[data['boltwood wind units']]
+        if 'boltwood rain status' in data.keys():
+            data['boltwood rain status string'] = rain_status[data['boltwood rain status']]
+        if 'boltwood wet status' in data.keys():
+            data['boltwood wet status string'] = wet_status[data['boltwood wet status']]
+        if 'boltwood cloud condition' in data.keys():
+            data['boltwood cloud condition string'] = cloud_condition[data['boltwood cloud condition']]
+        if 'boltwood wind condition' in data.keys():
+            data['boltwood wind condition string'] = wind_condition[data['boltwood wind condition']]
+        if 'boltwood rain condition' in data.keys():
+            data['boltwood rain condition string'] = rain_condition[data['boltwood rain condition']]
+        if 'boltwood day condition' in data.keys():
+            data['boltwood day condition string'] = day_condition[data['boltwood day condition']]
+        if 'boltwood roof close' in data.keys():
+            data['boltwood roof close string'] = roof_close[data['boltwood roof close']]
 
         clarity_time = datetime.datetime.strptime('{} {}'.format(entries[-1]['boltwood date'], entries[-1]['boltwood time'][:-3]), '%Y-%m-%d %H:%M:%S')
         clarity_age = (now - clarity_time).total_seconds()
 
-        data_time = datetime.datetime.strptime('{} {}'.format(entries[-1]['UT date'], entries[-1]['UT time']), '%Y%m%dUT %H:%M:%S')
+        data_time = datetime.datetime.strptime('{} {}'.format(data['UT date'], data['UT time']), '%Y%m%dUT %H:%M:%S')
         data_age = (nowut - data_time).total_seconds()
 
         self.render("status.html", title="VYSOS Status",\
@@ -41,7 +68,7 @@ class StatusHandler(RequestHandler):
                     time = nowut.strftime('%H:%M:%S'),\
                     clarity_age = clarity_age,\
                     data_age = data_age,\
-                    data = entries[-1],\
+                    data = data,\
                     )
 
 def make_app():
