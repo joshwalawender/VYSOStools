899f429edfbb8eabbd493b65db8092e8b1b34c3c
Josh Walawender <jmwalawender@gmail.com>
2015-03-12 21:35:52 -1000

    Fix to handle new config file names

--- scripts/MakeNightlyPlots.py
+++ scripts/MakeNightlyPlots.py
@@ -400,9 +400,9 @@ def MakePlots(DateString, telescope, logger):
 
 
     if telescope == 'V5':
-        config_file = os.path.join(os.path.expanduser('~'), 'IQMon', 'config_VYSOS-5.yaml')
+        config_file = os.path.join(os.path.expanduser('~'), 'IQMon', 'VYSOS5.yaml')
     if telescope == 'V20':
-        config_file = os.path.join(os.path.expanduser('~'), 'IQMon', 'config_VYSOS-20.yaml')
+        config_file = os.path.join(os.path.expanduser('~'), 'IQMon', 'VYSOS20.yaml')
     tel = IQMon.Telescope(config_file)
 
 
