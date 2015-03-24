48717f80b924fa8a403cb49f687586ce927e3a51
Josh Walawender <jmwalawender@gmail.com>
2015-03-24 12:21:30 -1000

    Add make nightly plots to each day and add logger

--- scripts/process_dates.py
+++ scripts/process_dates.py
@@ -1,31 +1,34 @@
 import sys
 import os
 import re
+import logging
 from glob import glob
 from datetime import datetime as dt
 from datetime import timedelta as tdelta
 from argparse import ArgumentParser
+import subprocess
 
 import measure_image
+import make_nightly_plots
 
-def main(startdate, enddate):
+def main(startdate, enddate, logger):
     MatchFilename = re.compile("(.*)\-([0-9]{8})at([0-9]{6})\.fts")
     MatchEmpty = re.compile(".*\-Empty\-.*\.fts")
     oneday = tdelta(1, 0)
     now = startdate
     while now <= enddate:
         date_string = now.strftime('%Y%m%dUT')
-        print('Checking for images from {}'.format(date_string))
+        logger.info('Checking for images from {}'.format(date_string))
         images = []
         V5_path = os.path.join("/Volumes", "Drobo", "V5", "Images", date_string)
         V20_path = os.path.join("/Volumes", "Drobo", "V20", "Images", date_string)
         if os.path.exists(V5_path):
             V5_images = glob(os.path.join(V5_path, '*.fts'))
-            print('  Found {} images for the night of {} for V5'.format(len(V5_images), date_string))
+            logger.info('  Found {} images for the night of {} for V5'.format(len(V5_images), date_string))
             images.extend(V5_images)
         if os.path.exists(V20_path):
             V20_images = glob(os.path.join(V20_path, '*.fts'))
-            print('  Found {} images for the night of {} for V20'.format(len(V20_images), date_string))
+            logger.info('  Found {} images for the night of {} for V20'.format(len(V20_images), date_string))
             images.extend(V20_images)
         for image in images:
             if MatchFilename.match(image) and not MatchEmpty.match(image):
@@ -35,12 +38,13 @@ def main(startdate, enddate):
                                  zero_point=True,\
                                  analyze_image=True)
                 except:
-                    print('WARNING:  MeasureImage failed on {}'.format(image))
+                    logger.warning('MeasureImage failed on {}'.format(image))
                     measure_image.MeasureImage(image,\
                                  clobber_logs=False,\
                                  zero_point=False,\
                                  analyze_image=False)
-
+        make_nightly_plots.make_plots(date_string, 'V5', logger)
+        make_nightly_plots.make_plots(date_string, 'V20', logger)
         now += oneday
 
     
@@ -52,6 +56,11 @@ if __name__ == "__main__":
     ##-------------------------------------------------------------------------
     ## create a parser object for understanding command-line arguments
     parser = ArgumentParser(description="Describe the script")
+    ## add flags
+    parser.add_argument("-v", "--verbose",
+        action="store_true", dest="verbose",
+        default=False, help="Be verbose! (default = False)")
+    ## add arguments
     parser.add_argument("-s", "--start", 
         dest="start", required=True, type=str,
         help="UT date of first night to analyze. (i.e. '20130805UT')")
@@ -60,7 +69,31 @@ if __name__ == "__main__":
         help="UT date of last night to analyze. (i.e. '20130805UT')")
     args = parser.parse_args()
 
+    ##-------------------------------------------------------------------------
+    ## Create Logger Object
+    ##-------------------------------------------------------------------------
+    logger = logging.getLogger('process_dates')
+    logger.setLevel(logging.DEBUG)
+    ## Set up console output
+    LogConsoleHandler = logging.StreamHandler()
+    if args.verbose:
+        LogConsoleHandler.setLevel(logging.DEBUG)
+    else:
+        LogConsoleHandler.setLevel(logging.INFO)
+    LogFormat = logging.Formatter('%(asctime)23s %(levelname)8s: %(message)s')
+    LogConsoleHandler.setFormatter(LogFormat)
+    logger.addHandler(LogConsoleHandler)
+    ## Set up file output
+#     LogFilePath = os.path.join()
+#     if not os.path.exists(LogFilePath):
+#         os.mkdir(LogFilePath)
+#     LogFile = os.path.join(LogFilePath, 'get_status.log')
+#     LogFileHandler = logging.FileHandler(LogFile)
+#     LogFileHandler.setLevel(logging.DEBUG)
+#     LogFileHandler.setFormatter(LogFormat)
+#     logger.addHandler(LogFileHandler)
+
     startdate = dt.strptime(args.start, '%Y%m%dUT')
     enddate = dt.strptime(args.end, '%Y%m%dUT')
 
-    main(startdate, enddate)
+    main(startdate, enddate, logger)
