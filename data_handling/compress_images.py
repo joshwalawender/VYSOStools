7d35b53cb50c116e12271affef6efdea0d79fe36
Josh Walawender <jmwalawender@gmail.com>
2016-12-28 09:02:55 -1000

    Update compression script to generically handle folders of images

--- data_handling/compress_images.py
+++ data_handling/compress_images.py
@@ -33,38 +33,17 @@ def compress(file):
             os.remove(file)
 
 
-def compress_files(telescope, date):
-    assert re.match('\d{8}UT', date)
-    drobo_path = '/Volumes/Drobo/{}/Images/{}'.format(telescope, date)
-    ext_path = '/Volumes/WD500B/{}/Images/{}'.format(telescope, date)
-    files = glob(os.path.join(ext_path, '*.fts'))
-    files.extend(glob(os.path.join(ext_path, '*.fits')))
-    files.extend(glob(os.path.join(drobo_path, '*.fts')))
-    files.extend(glob(os.path.join(drobo_path, '*.fits')))
+def process_folder(path):
+    files = glob(os.path.join(path, '*.fts'))
+    files.extend(glob(os.path.join(path, '*.fits')))
     nfiles = len(files)
     print('Exmining {} files'.format(nfiles))
     for i,file in enumerate(files):
-        filename = os.path.basename(file)
-        drobo_file = os.path.join(drobo_path, filename)
         print('{}/{}: {}'.format(i+1, nfiles, file))
         compress(file)
 
 
 if __name__ == '__main__':
-    ##-------------------------------------------------------------------------
-    ## Parse Command Line Arguments
-    ##-------------------------------------------------------------------------
-    ## create a parser object for understanding command-line arguments
-    parser = argparse.ArgumentParser(
-             description="Program description.")
-    ## add flags
-    parser.add_argument("-t", "--telescope",
-        dest="telescope", required=True, type=str,
-        choices=["V5", "V20"],
-        help="Telescope which took the data ('V5' or 'V20')")
-    parser.add_argument("-d", "--date",
-        type=str, dest="date",
-        help="The date to copy (in YYYYMMDDUT format)")
-    args = parser.parse_args()
-    
-    compress_files(args.telescope, args.date)
+    for folder in glob('/Volumes/MLOData/uncompressed/V5/Images/20*/*'):
+        process_folder(folder)
+
