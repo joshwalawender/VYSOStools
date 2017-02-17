8dd7cca9cfb0d803f11962c496fb94b7003dbade
Josh Walawender <jmwalawender@gmail.com>
2017-02-16 20:50:26 -1000

    Update telstatus for get_status.py changes

--- webpage/custom_handlers.py
+++ webpage/custom_handlers.py
@@ -79,10 +79,10 @@ class telstatus(me.Document):
     focuser_position = me.DecimalField(min_value=0, max_value=105000, precision=1)
 
     ## RCOS TCC Status
-    fan_speed = me.DecimalField(min_value=0, max_value=100, precision=0)
+    fan_speed = me.DecimalField(min_value=0, max_value=100, precision=1)
     truss_temperature  = me.DecimalField(min_value=-50, max_value=120, precision=1)
     primary_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
-    secondary_temperature = me.DecimalField(min_value=-50, max_value=120, precision=0)
+    secondary_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
     ## CBW Status
     dome_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
     fan_state = me.BooleanField()
@@ -92,7 +92,7 @@ class telstatus(me.Document):
             'indexes': ['telescope', 'current', 'date']}
 
     def __str__(self):
-        output = 'MongoEngine Document for: {}\n'.format(self.date)
+        output = 'MongoEngine Document at: {}\n'.format(self.date.strftime('%Y%m%d %H:%M:%S'))
         if self.telescope: output += '  Telescope: {}\n'.format(self.telescope)
         if self.current: output += '  Current: {}\n'.format(self.current)
         if self.connected: output += '  connected: {}\n'.format(self.connected)
@@ -104,8 +104,15 @@ class telstatus(me.Document):
         if self.RA: output += '  RA: {:.4f}\n'.format(self.RA)
         if self.DEC: output += '  DEC: {:.4f}\n'.format(self.DEC)
         if self.ACPerr: output += '  ACPerr: {}\n'.format(self.ACPerr)
-        if self.focuser_temperature: output += '  focuser_temperature: {}\n'.format(self.focuser_temperature)
+        if self.focuser_temperature: output += '  focuser_temperature: {:.1f}\n'.format(self.focuser_temperature)
         if self.focuser_position: output += '  focuser_position: {}\n'.format(self.focuser_position)
+        if self.truss_temperature: output += '  truss_temperature: {:.1f}\n'.format(self.truss_temperature)
+        if self.primary_temperature: output += '  primary_temperature: {:.1f}\n'.format(self.primary_temperature)
+        if self.secondary_temperature: output += '  secondary_temperature: {:.1f}\n'.format(self.secondary_temperature)
+        if self.fan_speed: output += '  fan_speed: {}\n'.format(self.fan_speed)
+        if self.dome_temperature: output += '  dome_temperature: {:.1f}\n'.format(self.dome_temperature)
+        if self.fan_state: output += '  fan_state: {}\n'.format(self.fan_state)
+        if self.fan_enable: output += '  fan_enable: {}\n'.format(self.fan_enable)
         return output
 
     def __repr__(self):
