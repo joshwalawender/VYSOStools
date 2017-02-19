2258a1ab68e7fcff4a4977aa33775f4f799becaa
Josh Walawender <jmwalawender@gmail.com>
2017-02-19 09:03:51 -1000

    Add weather schema

--- scripts/schema.py
+++ scripts/schema.py
@@ -1,33 +1,55 @@
 import datetime
 import mongoengine as me
 
-class telstatus(me.Document):
-    telescope = me.StringField(max_length=3, required=True, choices=['V5', 'V20'])
-    date = me.DateTimeField(default=datetime.datetime.utcnow(), required=True)
-    current = me.BooleanField(default=True, required=True)
-    ## ACP Status
-    connected = me.BooleanField()
-    park = me.BooleanField()
-    slewing = me.BooleanField()
-    tracking = me.BooleanField()
-    alt = me.DecimalField(min_value=0, max_value=90, precision=4)
-    az = me.DecimalField(min_value=0, max_value=360, precision=4)
-    RA = me.DecimalField(min_value=0, max_value=360, precision=4)
-    DEC = me.DecimalField(min_value=-90, max_value=90, precision=4)
-    ACPerr = me.StringField(max_length=256)
-    ## FocusMax Status
-    focuser_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
-    focuser_position = me.DecimalField(min_value=0, max_value=105000, precision=1)
+class weatherbase(me.Document):
+    querydate = me.DateTimeField(default=datetime.datetime.utcnow(), required=True)
+    date = me.DateTimeField(required=True)
+    clouds = me.DecimalField(precision=2)
+    temp = me.DecimalField(precision=2)
+    wind = me.DecimalField(precision=1)
+    gust = me.DecimalField(precision=1)
+    rain = me.IntField()
+    light = me.IntField()
+    switch = me.IntField()
+    safe = me.BooleanField()
 
-    ## RCOS TCC Status
-    fan_speed = me.DecimalField(min_value=0, max_value=100, precision=0)
-    truss_temperature  = me.DecimalField(min_value=-50, max_value=120, precision=1)
-    primary_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
-    secondary_temperature = me.DecimalField(min_value=-50, max_value=120, precision=0)
-    ## CBW Status
-    dome_temperature = me.DecimalField(min_value=-50, max_value=120, precision=1)
-    fan_state = me.BooleanField()
-    fan_enable = me.BooleanField()
+    meta = {'allow_inheritance': True,
+            'collection': 'weather'}
 
-    meta = {'collection': 'telstatus',
-            'indexes': ['telescope', 'current', 'date']}
+    def __str__(self):
+        output = 'MongoEngine Document at: {}\n'.format(
+                 self.querydate.strftime('%Y%m%d %H:%M:%S'))
+        if self.date: output += '  Date: {}\n'.format(
+                      self.date.strftime('%Y%m%d %H:%M:%S'))
+        if self.clouds: output += '  clouds: {:.2f}\n'.format(self.clouds)
+        if self.temp: output += '  temp: {:.2f}\n'.format(self.temp)
+        if self.wind: output += '  wind: {:.1f}\n'.format(self.wind)
+        if self.gust: output += '  gust: {:.1f}\n'.format(self.gust)
+        if self.rain: output += '  rain: {:.0f}\n'.format(self.rain)
+        if self.light: output += '  light: {:.0f}\n'.format(self.light)
+        if self.switch: output += '  switch: {}\n'.format(self.switch)
+        if self.safe: output += '  safe: {}\n'.format(self.safe)
+        return output
+
+    def __repr__(self):
+        return self.__str__()
+
+class weather(weatherbase):
+    meta = {'collection': 'weather',
+            'indexes': ['date']}
+
+class currentweather(weatherbase):
+    meta = {'collection': 'currentweather'}
+
+    def from_weather(self, wd):
+        self.querydate = wd.querydate
+        self.date      = wd.date
+        self.clouds    = wd.clouds
+        self.temp      = wd.temp
+        self.wind      = wd.wind
+        self.gust      = wd.gust
+        self.rain      = wd.rain
+        self.light     = wd.light
+        self.switch    = wd.switch
+        self.safe      = wd.safe
+
