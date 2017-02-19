c0fd5a881ab0758a23f6225885dc9a00e82711af
Josh Walawender <jmwalawender@gmail.com>
2017-02-19 10:18:11 -1000

    Update web page for new weather schema

--- webpage/custom_handlers.py
+++ webpage/custom_handlers.py
@@ -26,39 +26,7 @@ import IQMon
 import datetime
 import mongoengine as me
 
-class weather(me.Document):
-    querydate = me.DateTimeField(default=datetime.datetime.utcnow(), required=True)
-    date = me.DateTimeField(required=True)
-    current = me.BooleanField(default=True, required=True)
-    clouds = me.DecimalField(precision=2)
-    temp = me.DecimalField(precision=2)
-    wind = me.DecimalField(precision=1)
-    gust = me.DecimalField(precision=1)
-    rain = me.IntField()
-    light = me.IntField()
-    switch = me.IntField()
-    safe = me.BooleanField()
-
-    meta = {'collection': 'weather',
-            'indexes': ['current', 'querydate', 'date']}
-
-    def __str__(self):
-        output = 'MongoEngine Document at: {}\n'.format(self.querydate.strftime('%Y%m%d %H:%M:%S'))
-        if self.date: output += '  Date: {}\n'.format(self.date.strftime('%Y%m%d %H:%M:%S'))
-        if self.current: output += '  Current: {}\n'.format(self.current)
-        if self.clouds: output += '  clouds: {:.2f}\n'.format(self.clouds)
-        if self.temp: output += '  temp: {:.2f}\n'.format(self.temp)
-        if self.wind: output += '  wind: {:.1f}\n'.format(self.wind)
-        if self.gust: output += '  gust: {:.1f}\n'.format(self.gust)
-        if self.rain: output += '  rain: {:.0f}\n'.format(self.rain)
-        if self.light: output += '  light: {:.0f}\n'.format(self.light)
-        if self.switch: output += '  switch: {}\n'.format(self.switch)
-        if self.safe: output += '  safe: {}\n'.format(self.safe)
-        return output
-
-    def __repr__(self):
-        return self.__str__()
-
+from VYSOS.schema import weather, currentweather
 
 class telstatus(me.Document):
     telescope = me.StringField(max_length=3, required=True, choices=['V5', 'V20'])
@@ -277,7 +245,10 @@ class Status(RequestHandler):
             tlog.app_log.info('{} {}'.format(v20status.RA, v20status.DEC))
             tlog.app_log.info('{}'.format(v20coord.to_string('hmsdms', sep=':', precision=0).split()))
             v20status.RA, v20status.DEC = v20coord.to_string('hmsdms', sep=':', precision=0).split()
-        currentweather = weather.objects(__raw__={'current': True})[0]
+
+        cw = currentweather.objects()
+        tlog.app_log.info('  Found {:d} current weather docs'.format(cw.count()))
+
         cctv = False
         if input.lower() in ["cctv", "cctv.html"]:
             cctv = True
@@ -295,6 +266,6 @@ class Status(RequestHandler):
                     v5_nflats = get_nflats('V5', link_date_string),\
                     v20_nflats = get_nflats('V20', link_date_string),\
                     cctv=cctv,
-                    currentweather=currentweather,
+                    currentweather=cw[0],
                     )
         tlog.app_log.info('  Done')
