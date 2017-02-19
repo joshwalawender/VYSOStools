6f007b0d294e6330e85c86dab823df6174a1d479
Josh Walawender <jmwalawender@gmail.com>
2017-02-19 10:19:30 -1000

    Update setup.py for new directory structure

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 setup(
     name = "VYSOS",
-    version = "1.0",
+    version = "1.1.2",
     author='Josh Walawender',
     packages = find_packages(),
-    entry_points = {
-        'console_scripts': [
-            'measureimage = scripts.measure_image:main',
-            'measurenight = scripts.measure_night:main',
-            'monitor = scripts.watch_directory:main',
-            'makenightlyplots = scripts.make_nightly_plots:main',
-            'cleanupIQMon = scripts.remove_old_plots:main',
-            'copydatalocal = data_handling.copy_data_local:main',
-            'copydataremote = data_handling.copy_data_remote:main',
-        ]}
+#     entry_points = {
+#         'console_scripts': [
+#             'measureimage = scripts.measure_image:main',
+#             'measurenight = scripts.measure_night:main',
+#             'monitor = scripts.watch_directory:main',
+#             'makenightlyplots = scripts.make_nightly_plots:main',
+#             'cleanupIQMon = scripts.remove_old_plots:main',
+#             'copydatalocal = data_handling.copy_data_local:main',
+#             'copydataremote = data_handling.copy_data_remote:main',
+#         ]}
 )
