3d1a9998fd6c11cc521721f48d30be175cc82ec6
Josh Walawender <jmwalawender@gmail.com>
2017-07-09 12:53:53 -1000

    Remove use of mongoengine

--- VYSOS/get_status.py
+++ VYSOS/get_status.py
@@ -11,9 +11,13 @@ import time
 import datetime
 import re
 import numpy as np
+import pymongo
 
-import mongoengine as me
-from VYSOS.schema import telstatus
+import win32com.client
+import pywintypes
+
+# import mongoengine as me
+# from VYSOS.schema import telstatus
 
 ##-------------------------------------------------------------------------
 ## Query ASCOM ACPHub for Telescope Position and State
@@ -27,32 +31,32 @@ def get_telescope_info(status, logger):
         return status
 
     try:
-        status.connected = ACP.Connected
-        logger.debug('  ACP Connected = {}'.format(status.connected))
-        if status.connected:
-            status.park = ACP.AtPark
-            logger.info('  ACP At Park = {}'.format(status.park))
-            status.slewing = ACP.Slewing
-            logger.info('  ACP Slewing = {}'.format(status.slewing))
-            status.tracking = ACP.Tracking
-            logger.info('  ACP Tracking = {}'.format(status.tracking))
-            status.alt = float(ACP.Altitude)
-            logger.info('  ACP Alt = {:.2f}'.format(status.alt))
-            status.az = float(ACP.Azimuth)
-            logger.info('  ACP Az = {:.2f}'.format(status.az))
+        status['connected'] = ACP.Connected
+        logger.debug('  ACP Connected = {}'.format(status['connected']))
+        if status['connected']:
+            status['park'] = ACP.AtPark
+            logger.info('  ACP At Park = {}'.format(status['park']))
+            status['slewing'] = ACP.Slewing
+            logger.info('  ACP Slewing = {}'.format(status['slewing']))
+            status['tracking'] = ACP.Tracking
+            logger.info('  ACP Tracking = {}'.format(status['tracking']))
+            status['alt'] = float(ACP.Altitude)
+            logger.info('  ACP Alt = {:.2f}'.format(status['alt']))
+            status['az'] = float(ACP.Azimuth)
+            logger.info('  ACP Az = {:.2f}'.format(status['az']))
             try:
-                status.RA = ACP.TargetRightAscension * 15.0
-                status.DEC = ACP.TargetDeclination
-                logger.info('  ACP target RA = {:.4f}'.format(status.RA))
-                logger.info('  ACP target Dec = {:.4f}'.format(status.DEC))
+                status['RA'] = ACP.TargetRightAscension * 15.0
+                status['DEC'] = ACP.TargetDeclination
+                logger.info('  ACP target RA = {:.4f}'.format(status['RA']))
+                logger.info('  ACP target Dec = {:.4f}'.format(status['DEC']))
             except:
                 logger.info('  Could not get target info')
     except pywintypes.com_error as err:
         logger.warning('COM error:')
         logger.warning('  {}'.format(err.message))
-        status.ACPerr = '{}'.format(err.message)
+        status['ACPerr'] = '{}'.format(err.message)
     except:
-        status.connected = False
+        status['connected'] = False
         logger.warning('Queries to ACP object failed')
 
     return status
@@ -88,12 +92,12 @@ def get_focuser_info(status, logger):
         ## Filter out bad values
         median_temp = np.median(FocusMax_Temps)
         if (median_temp > -10) and (median_temp < 150):
-            status.focuser_temperature = median_temp
-            logger.info('  FocusMax temperature = {:.1f} {}'.format(status.focuser_temperature, 'C'))
+            status['focuser_temperature'] = median_temp
+            logger.info('  FocusMax temperature = {:.1f} {}'.format(status['focuser_temperature'], 'C'))
     ## Get Position
     try:
-        status.focuser_position = int(FocusMax.Position)
-        logger.info('  FocusMax position = {:d}'.format(status.focuser_position))
+        status['focuser_position'] = int(FocusMax.Position)
+        logger.info('  FocusMax position = {:d}'.format(status['focuser_position']))
     except:
         pass
 
@@ -151,20 +155,20 @@ def get_RCOS_info(status, logger):
             pass
         time.sleep(1)
     if len(RCOS_Truss_Temps) >= 3:
-        status.truss_temperature = (float(np.median(RCOS_Truss_Temps)) - 32.)/1.8
+        status['truss_temperature'] = (float(np.median(RCOS_Truss_Temps)) - 32.)/1.8
         logger.info('  RCOS temperature (truss) = {:.1f} {}'.format(
-                    status.truss_temperature, 'C'))
+                    status['truss_temperature'], 'C'))
     if len(RCOS_Primary_Temps) >= 3:
-        status.primary_temperature = (float(np.median(RCOS_Primary_Temps)) - 32.)/1.8
+        status['primary_temperature'] = (float(np.median(RCOS_Primary_Temps)) - 32.)/1.8
         logger.info('  RCOS temperature (primary) = {:.1f} {}'.format(
-                    status.primary_temperature, 'C'))
+                    status['primary_temperature'], 'C'))
     if len(RCOS_Secondary_Temps) >= 3:
-        status.secondary_temperature = (float(np.median(RCOS_Secondary_Temps)) - 32.)/1.8
+        status['secondary_temperature'] = (float(np.median(RCOS_Secondary_Temps)) - 32.)/1.8
         logger.info('  RCOS temperature (secondary) = {:.1f} {}'.format(
-                    status.secondary_temperature, 'C'))
+                    status['secondary_temperature'], 'C'))
     if len(RCOS_Fan_Speeds) >= 3:
-        status.fan_speed = float(np.median(RCOS_Fan_Speeds))
-        logger.info('  RCOS fan speed = {:.0f} %'.format(status.fan_speed))
+        status['fan_speed'] = float(np.median(RCOS_Fan_Speeds))
+        logger.info('  RCOS fan speed = {:.0f} %'.format(status['fan_speed']))
 
     return status
 
@@ -189,128 +193,22 @@ def control_by_web(status, logger):
         temp1 = temp1*9./5. + 32.
         tunits = 'F'
 
-    status.dome_temperature = temp1
-    status.fan_state = r1state
-    status.fan_enable = r2state
-
-
-
-
-
-#     import urllib
-#     from xml.dom import minidom
-#     logger.info('Getting CBW temperature module status')
-#     if ('RCOS temperature units' in focuser_info.keys()) and ('RCOS temperature (truss)' in focuser_info.keys()):
-#         if focuser_info['RCOS temperature units'] == 'F':
-#             InsideTemp = focuser_info['RCOS temperature (truss)']
-#         else:
-#             logger.error('  Focuser temperature unit mismatch')
-#             return {}
-#     else:
-#         logger.error('  Focuser temperature unit not found')
-#         return {}
-# 
-#     if ('boltwood temp units' in boltwood.keys()) and ('boltwood ambient temp' in boltwood.keys()):
-#         if boltwood['boltwood temp units'] == 'F':
-#             OutsideTemp = boltwood['boltwood ambient temp']
-#         else:
-#             logger.error('  Boltwood temperature unit mismatch')
-#             return{}
-#     else:
-#         logger.error('  Boltwood temperature unit not found')
-#         return{}
-# 
-#     CBW_info = {}
-#     IPaddress = "192.168.1.115"
-#     try:
-#         page = urllib.urlopen("http://"+IPaddress+"/state.xml")
-#         contents = page.read()
-#         ContentLines = contents.splitlines()
-#         xmldoc = minidom.parseString(contents)
-#         CBW_info['CBW temperature units'] = str(xmldoc.getElementsByTagName('units')[0].firstChild.nodeValue)
-#         CBW_info['CBW temp1'] = float(xmldoc.getElementsByTagName('sensor1temp')[0].firstChild.nodeValue)
-#         logger.debug('  Temp1 = {:.1f} {}'.format(CBW_info['CBW temp1'], CBW_info['CBW temperature units']))
-#         CBW_info['CBW temp2'] = float(xmldoc.getElementsByTagName('sensor2temp')[0].firstChild.nodeValue)
-#         logger.debug('  Temp2 = {:.1f} {}'.format(CBW_info['CBW temp2'], CBW_info['CBW temperature units']))
-#         if CBW_info['CBW temperature units'] == "C":
-#             CBW_info['CBW temp1'] = CBW_info['CBW temp1']*9./5. + 32.
-#             CBW_info['CBW temp2'] = CBW_info['CBW temp2']*9./5. + 32.
-#             CBW_info['CBW temperature units'] = 'F'
-#             logger.info('  Temp1 = {:.1f} {}'.format(CBW_info['CBW temp1'], CBW_info['CBW temperature units']))
-#             logger.debug('  Temp2 = {:.1f} {}'.format(CBW_info['CBW temp2'], CBW_info['CBW temperature units']))
-#         CBW_info['CBW fan state'] = bool(xmldoc.getElementsByTagName('relay1state')[0].firstChild.nodeValue)
-#         logger.info('  Fans On? = {}'.format(CBW_info['CBW fan state']))
-#         CBW_info['CBW fan enable'] = bool(xmldoc.getElementsByTagName('relay2state')[0].firstChild.nodeValue)
-#         logger.debug('  Fan Control Enabled? = {}'.format(CBW_info['CBW fan enable']))
-#     except:
-#         logger.error('Could not connect to CBW temperature module.')
-#         return {}
-# 
-#     ## Control Fans
-#     DeadbandHigh = 0.5
-#     DeadbandLow = 3.0
-# 
-#     ## If fan enable not on, return values and stop
-#     if not CBW_info['CBW fan enable']:
-#         if CBW_info['CBW fan state']:
-#             logger.info("  Turning Dome Fan Off.  Remote Control Set to Off.")
-#             page = urllib.urlopen("http://"+IPaddress+"/state.xml?relay1State=0")
-#         return CBW_info
-#     
-#     ## If fans should be on or off based on the time of day
-#     operate_fans_now = True
-#     
-#     ## Set state of fans based on temperature
-#     if OutsideTemp and InsideTemp:
-#         logger.debug('  Inside Temp = {:.1f}'.format(InsideTemp))
-#         logger.debug('  Outside Temp = {:.1f}'.format(OutsideTemp))
-#         DeltaT = InsideTemp - OutsideTemp
-# 
-#         ## Turn on Fans if Inside Temperature is High
-#         if operate_fans_now and (InsideTemp > OutsideTemp + DeadbandHigh):
-#             if not CBW_info['CBW fan state']:
-#                 logger.info("  Turning Dome Fan On.  DeltaT = %.1f" % DeltaT)
-#                 page = urllib.urlopen("http://"+IPaddress+"/state.xml?relay1State=1")
-#                 CBW_info['CBW fan state'] = True
-#             else:
-#                 logger.debug("  Leaving Dome Fan On.  DeltaT = %.1f" % DeltaT)
-#         ## Turn off Fans if Inside Temperature is Low
-#         elif operate_fans_now and (InsideTemp < OutsideTemp - DeadbandLow):
-#             if CBW_info['CBW fan state']:
-#                 logger.info("  Turning Dome Fan Off.  DeltaT = %.1f" % DeltaT)
-#                 page = urllib.urlopen("http://"+IPaddress+"/state.xml?relay1State=0")
-#                 CBW_info['CBW fan state'] = False
-#             else:
-#                 logger.debug("  Leaving Dome Fan Off.  DeltaT = %.1f" % DeltaT)
-#         ## Turn off Fans if it is night
-#         elif not operate_fans_now:
-#             if CBW_info['CBW fan state']:
-#                 logger.info("  Turning Dome Fan Off for Night.  DeltaT = %.1f" % DeltaT)
-#                 page = urllib.urlopen("http://"+IPaddress+"/state.xml?relay1State=0")
-#                 CBW_info['CBW fan state'] = False
-#             else:
-#                 logger.debug("  Leaving Dome Fan Off.  DeltaT = %.1f" % DeltaT)
+    status['dome_temperature'] = temp1
+    status['fan_state'] = r1state
+    status['fan_enable'] = r2state
 
     return status
 
 
 def get_status_and_log(telescope, logger):
-    ## Set up log file output
-#     LogFilePath = os.path.join('Z:\\', 'Logs', DateString)
-#     if not os.path.exists(LogFilePath):
-#         os.mkdir(LogFilePath)
-#     LogFile = os.path.join(LogFilePath, 'get_status_{}.log'.format(DateString))
-#     LogFileHandler = logging.FileHandler(LogFile)
-#     LogFileHandler.setLevel(logging.DEBUG)
-#     LogFileHandler.setFormatter(LogFormat)
-#     logger.addHandler(LogFileHandler)
 
     ##-------------------------------------------------------------------------
     ## Get Status Info
     ##-------------------------------------------------------------------------
     logger.info('#### Starting Status Queries ####')
-    status = telstatus(telescope=telescope, current=True,
-                       date=datetime.datetime.utcnow())
+    status = {'telescope': telescope,
+              'date':datetime.datetime.utcnow()
+             }
     status = get_telescope_info(status, logger)
     status = get_focuser_info(status, logger)
     if telescope == 'V20':
@@ -323,37 +221,24 @@ def get_status_and_log(telescope, logger):
     while done is False:
         try:
             logger.info('Connecting to mongo db at 192.168.1.101')
-            me.connect('vysos', host='192.168.1.101')
+            client = pymongo.MongoClient('192.168.1.101', 27017)
+            db = client.vysos
+            status_collection = db['{}status'.format(telescope)]
         except:
             logger.error('Failed to connect to mongo')
-            logger.error('Will try again in 10 seconds')
-            time.sleep(10)
         else:
-            ## Edit old "current" entry to be not current
-            ncurrent = len(telstatus.objects(__raw__={'current': True, 'telescope': telescope}))
-            if ncurrent < 1:
-                logger.error('No exiting "current" document found!')
-            elif ncurrent == 1:
-                logger.info('Modifying old "current" document')
-                telstatus.objects(__raw__={'current': True, 'telescope': telescope}).update_one(set__current=False)
-                logger.info('  Done')
-            else:
-                logger.error('Multiple ({}) existing "current" documents found!'.format(ncurrent))
-                logger.info('Updating old "current" documents')
-                telstatus.objects(__raw__={'current': True, 'telescope': telescope}).update(set__current=False, multi=True)
-                logger.info('  Done')
-
             ## Save new "current" document
             try:
-                logger.info('Saving new "current" document')
-                status.save()
-                logger.info("  Done")
-                logger.info("\n{}".format(status))
+                inserted_id = status_collection.insert_one(status).inserted_id
+                logger.info("  Inserted document id: {}".format(inserted_id))
                 done = True
             except:
+                e = sys.exc_info()[0]
                 logger.error('Failed to add new document')
-                logger.error('Will try again in 10 seconds')
+                logger.error('Will wait 10 seconds and try again')
+                logger.error(e)
                 time.sleep(10)
+            client.close()
 
 
 if __name__ == '__main__':
@@ -398,12 +283,9 @@ if __name__ == '__main__':
         logger.addHandler(LogConsoleHandler)
 
 
-    while True:
-        if telescope in ['V5', 'V20']:
-            import win32com.client
-            import pywintypes
-            get_status_and_log(telescope, logger)
-        else:
-            get_weather(logger)
+    run = True
+    while run:
+        get_status_and_log(telescope, logger)
+#         run = False
         logging.shutdown()
         time.sleep(20)
