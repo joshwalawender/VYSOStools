ddcf60445674531bc33cfdffed10b82119587f4b
Josh Walawender <jmwalawender@gmail.com>
2017-08-05 21:13:38 -1000

    Update to use HTML table

--- dash/status.py
+++ dash/status.py
@@ -7,42 +7,51 @@ import pymongo
 from datetime import datetime as dt
 from datetime import timedelta as tdelta
 
-from VYSOS.schema import weather_limits
+from VYSOS import weather_limits, styles
 
+##-------------------------------------------------------------------------
+## Define App
+##-------------------------------------------------------------------------
 app = dash.Dash(__name__)
 app.layout = html.Div(
     html.Div([
         html.Div(id='live-update-text'),
-#         dcc.Graph(id='live-update-graph'),
         dcc.Interval(
             id='interval-component',
-            interval=10*1000 # in milliseconds
+            interval=5*1000 # in milliseconds
         )
     ])
 )
 
+
+##-------------------------------------------------------------------------
+## Get Weather Data
+##-------------------------------------------------------------------------
 def retrieve_weather(lookbackdays=0):
     delta_time = tdelta(lookbackdays, 120)
     client = pymongo.MongoClient('192.168.1.101', 27017)
     db = client.vysos
     weather = db.weather
-    data = [x for x in weather.find( {"date": {"$gt": dt.utcnow()-delta_time} } )]
+    weatherdata = [x for x in weather.find( {"date": {"$gt": dt.utcnow()-delta_time} } )]
     client.close()
-    return data
+    return weatherdata
 
-def get_conditions(data):
+##-------------------------------------------------------------------------
+## Determine Conditions from Weather Data
+##-------------------------------------------------------------------------
+def get_conditions(weatherdata):
     condition = {}
     color = {}
 
     # Determine Cloud Condition String
-    if data['clouds'] < weather_limits['Cloudiness (C)'][0]:
+    if weatherdata['clouds'] < weather_limits['Cloudiness (C)'][0]:
         condition['cloud'] = 'Clear'
         color['cloud'] = 'green'
-    elif data['clouds'] >= weather_limits['Cloudiness (C)'][0]\
-     and data['clouds'] < weather_limits['Cloudiness (C)'][1]:
+    elif weatherdata['clouds'] >= weather_limits['Cloudiness (C)'][0]\
+     and weatherdata['clouds'] < weather_limits['Cloudiness (C)'][1]:
         condition['cloud'] = 'Cloudy'
         color['cloud'] = 'yellow'
-    elif data['clouds'] >= weather_limits['Cloudiness (C)'][1]:
+    elif weatherdata['clouds'] >= weather_limits['Cloudiness (C)'][1]:
         condition['cloud'] = 'Overcast'
         color['cloud'] = 'red'
     else:
@@ -50,14 +59,14 @@ def get_conditions(data):
         color['cloud'] = 'red'
 
     # Determine Wind Condition String
-    if data['wind'] < weather_limits['Wind (kph)'][0]:
+    if weatherdata['wind'] < weather_limits['Wind (kph)'][0]:
         condition['wind'] = 'Calm'
         color['wind'] = 'green'
-    elif data['wind'] >= weather_limits['Wind (kph)'][0]\
-     and data['wind'] < weather_limits['Wind (kph)'][1]:
+    elif weatherdata['wind'] >= weather_limits['Wind (kph)'][0]\
+     and weatherdata['wind'] < weather_limits['Wind (kph)'][1]:
         condition['wind'] = 'Windy'
         color['wind'] = 'yellow'
-    elif data['wind'] >= weather_limits['Wind (kph)'][1]:
+    elif weatherdata['wind'] >= weather_limits['Wind (kph)'][1]:
         condition['wind'] = 'Very Windy'
         color['wind'] = 'red'
     else:
@@ -65,14 +74,14 @@ def get_conditions(data):
         color['wind'] = 'red'
 
     # Determine Gust Condition String
-    if data['gust'] < weather_limits['Wind (kph)'][0]:
+    if weatherdata['gust'] < weather_limits['Wind (kph)'][0]:
         condition['gust'] = 'Calm'
         color['gust'] = 'green'
-    elif data['gust'] >= weather_limits['Wind (kph)'][0]\
-     and data['gust'] < weather_limits['Wind (kph)'][1]:
+    elif weatherdata['gust'] >= weather_limits['Wind (kph)'][0]\
+     and weatherdata['gust'] < weather_limits['Wind (kph)'][1]:
         condition['gust'] = 'Windy'
         color['gust'] = 'yellow'
-    elif data['gust'] >= weather_limits['Wind (kph)'][1]:
+    elif weatherdata['gust'] >= weather_limits['Wind (kph)'][1]:
         condition['gust'] = 'Very Windy'
         color['gust'] = 'red'
     else:
@@ -80,10 +89,10 @@ def get_conditions(data):
         color['gust'] = 'red'
 
     # Determine Rain Condition String
-    if data['rain'] > weather_limits['Rain'][0]:
+    if weatherdata['rain'] > weather_limits['Rain'][0]:
         condition['rain'] = 'Dry'
         color['rain'] = 'green'
-    elif data['rain'] <= weather_limits['Rain'][0]:
+    elif weatherdata['rain'] <= weather_limits['Rain'][0]:
         condition['rain'] = 'Wet'
         color['rain'] = 'red'
     else:
@@ -93,6 +102,9 @@ def get_conditions(data):
     return condition, color
 
 
+##-------------------------------------------------------------------------
+## Get Telescope Status Data
+##-------------------------------------------------------------------------
 def retrieve_telstatus(telescope):
     client = pymongo.MongoClient('192.168.1.101', 27017)
     db = client.vysos
@@ -120,148 +132,167 @@ def retrieve_telstatus(telescope):
                 telstatus[telescope]['RA'], telstatus[telescope]['DEC'] = coord.to_string('hmsdms', sep=':', precision=0).split()
         else:
             telstatus[telescope] = {'date': dt.utcnow()-tdelta(365),
+                                    'status': 'Unknown',
                                     'connected': False}
+        ## Format Values and fill in missing keys
+        if 'alt' not in telstatus[telescope].keys():
+            telstatus[telescope]['altstr'] = ''
+        else:
+            telstatus[telescope]['altstr'] = '{:.1f} deg'.format(telstatus['V20']['alt'])
+        if 'az' not in telstatus[telescope].keys():
+            telstatus[telescope]['azstr'] = ''
+        else:
+            telstatus[telescope]['azstr'] = '{:.1f} deg'.format(telstatus['V20']['az'])
+        if 'RA' not in telstatus[telescope].keys():
+            telstatus[telescope]['RAstr'] = ''
+        else:
+            telstatus[telescope]['RAstr'] = '{}'.format(telstatus['V20']['RA'])
+        if 'Dec' not in telstatus[telescope].keys():
+            telstatus[telescope]['Decstr'] = ''
+        else:
+            telstatus[telescope]['Decstr'] = '{}'.format(telstatus['V20']['Dec'])
+        telstatus[telescope]['age'] = (dt.utcnow() - telstatus[telescope]['date']).total_seconds()/60.
     client.close()
 
     return telstatus
 
-
+##-------------------------------------------------------------------------
+## Run this to update the info on the page
+##-------------------------------------------------------------------------
 # The `dcc.Interval` component emits an event called "interval"
 # every `interval` number of milliseconds.
 # Subscribe to this event with the `events` argument of `app.callback`
 @app.callback(Output('live-update-text', 'children'),
               events=[Event('interval-component', 'interval')])
-def update_metrics():
-    data = retrieve_weather(lookbackdays=0)[-1]
-    condition, color = get_conditions(data)
+def generate_weather_table():
+    now = dt.now()
+    nowut = now + tdelta(0, 10*60*60)
+    weatherdata = retrieve_weather(lookbackdays=0)[-1]
+    condition, color = get_conditions(weatherdata)
+    weather_data_age = (nowut - weatherdata['date']).total_seconds()
+    if weather_data_age < 60:
+        weather_str = {True: 'Safe', False: 'Unsafe'}[weatherdata['safe']]
+        if weatherdata['safe'] is True:
+            weather_status = html.Span(weather_str, style={'color': 'green'})
+        else:
+            weather_status = html.Span(weather_str, style={'color': 'red'})
+    else:
+        weather_status = html.Span('Unsafe', style={'color': 'red'})
 
     telstatus = retrieve_telstatus('V20')
 
-    style = {'padding': '5px', 'fontSize': '16px'}
-    cloud_style = style.copy()
-    cloud_style.update({'color': color['cloud']})
-    wind_style = style.copy()
-    wind_style.update({'color': color['wind']})
-    gust_style = style.copy()
-    gust_style.update({'color': color['gust']})
-    rain_style = style.copy()
-    rain_style.update({'color': color['rain']})
-        
-    components = [
-        html.H2('VYSOS Weather'),
-        html.Span('Temperature:', style=style),
-        html.Span('{0:.1f} C,'.format(data['temp']), style=style),
-        html.Span('{0:.1f} F'.format(data['temp']*1.8+32.), style=style),
-        html.Br(),
-        html.Span('Cloudiness:', style=style),
-        html.Span(condition['cloud'], style=cloud_style),
-        html.Span('({0:.1f} F)'.format(data['clouds']*1.8+32.), style=style),
-        html.Br(),
-        html.Span('Wind Speed:', style=style),
-        html.Span(condition['wind'], style=wind_style),
-        html.Span('({0:.1f} kph)'.format(data['wind']), style=style),
-        html.Br(),
-        html.Span('Wind Gusts:', style=style),
-        html.Span(condition['gust'], style=gust_style),
-        html.Span('({0:.1f} kph)'.format(data['gust']), style=style),
-        html.Br(),
-        html.Span('Rain:', style=style),
-        html.Span(condition['rain'], style=rain_style),
-        html.Span('({0:.0f})'.format(data['rain']), style=style),
-        html.Hr(),
-    ]
+    tdcw300 = styles['tdc'].copy()
+    tdcw300['width'] = '300px'
+    tdrw200 = styles['tdc'].copy()
+    tdrw200['width'] = '200px'
+    tdlw150 = styles['tdl'].copy()
+    tdlw150['width'] = '150px'
+    tdcw250 = styles['tdc'].copy()
+    tdcw250['width'] = '250px'
 
-    components.append(html.H2('VYSOS-20 Status'))
+    weather_table = html.Table([
+                    html.Tr([
+                             html.Td(html.Span('Time', style={'font-weight': 'bold'}), style=tdcw300),
+                             html.Td(html.Span('Weather', style={'font-weight': 'bold'}), style=tdrw200),
+                             html.Td(html.Span(weather_status, style={'font-weight': 'bold'}), style=tdlw150),
+                             html.Td(html.Span('Disks', style={'font-weight': 'bold'}), style=tdcw250),
+                            ]),
+                    html.Tr([
+                             html.Td(now.strftime('%Y/%m/%d %H:%M:%S HST'), style=styles['tdl']),
+                             html.Td('Ambient Temperature', style=styles['tdr']),
+                             html.Td(f"{weatherdata['temp']:.1f} C, {weatherdata['temp']*1.8+32.:.1f} F", style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(nowut.strftime('%Y/%m/%d %H:%M:%S UT'), style=styles['tdl']),
+                             html.Td('Cloudiness', style=styles['tdr']),
+                             html.Td([html.Span(condition['cloud'], style={'color': color['cloud']}),
+                                      html.Span(' ({0:.1f} F)'.format(weatherdata['clouds']*1.8+32.), style=styles['p']),
+                                     ], style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(style=styles['tdl']),
+                             html.Td('Wind Speed', style=styles['tdr']),
+                             html.Td([html.Span(condition['wind'], style={'color': color['wind']}),
+                                      html.Span(' ({0:.1f} kph)'.format(weatherdata['wind']), style=styles['p']),
+                                     ], style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(style=styles['tdl']),
+                             html.Td('Gusts', style=styles['tdr']),
+                             html.Td([html.Span(condition['gust'], style={'color': color['gust']}),
+                                      html.Span(' ({0:.1f} kph)'.format(weatherdata['gust']), style=styles['p']),
+                                     ], style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(style=styles['tdl']),
+                             html.Td('Rain', style=styles['tdr']),
+                             html.Td([html.Span(condition['rain'], style={'color': color['rain']}),
+                                      html.Span(' ({0:.0f})'.format(weatherdata['rain']), style=styles['p']),
+                                     ], style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(style=styles['tdl']),
+                             html.Td('Weather Data Age', style=styles['tdr']),
+                             html.Td('{:.1f}s'.format(weather_data_age), style=styles['tdl']),
+                            ]),
+                    html.Tr([
+                             html.Td(html.Img(src='http://127.0.0.1:80/static/weather.png', width=800), colSpan=4, style=styles['tdc']),
+                            ]),
+                   ], style=styles['table'])
 
-    components.append(html.Span('ACP Connected:', style=style))
-    if 'connected' in telstatus['V20'].keys():
-        components.append(html.Span('{}'.format(telstatus['V20']['connected']), style=style))
-    components.append(html.Br())
+    telstatus_table = html.Table([
+                      html.Tr([
+                               html.Td(html.Span('Status', style={'font-weight': 'bold'}), style=styles['tdc']),
+                               html.Td(html.Span('VYSOS-5', style={'font-weight': 'bold'}), style=styles['tdc']),
+                               html.Td(html.Span('VYSOS-20', style={'font-weight': 'bold'}), style=styles['tdc']),
+                              ]),
+                      html.Tr([
+                               html.Td('ACP Connected', style=styles['tdr']),
+                               html.Td('{}'.format(telstatus['V5']['connected']), style=styles['tdl']),
+                               html.Td('{}'.format(telstatus['V20']['connected']), style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('Status', style=styles['tdr']),
+                               html.Td('{}'.format(telstatus['V5']['status']), style=styles['tdl']),
+                               html.Td('{}'.format(telstatus['V20']['status']), style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('Alt', style=styles['tdr']),
+                               html.Td(telstatus['V5']['altstr'], style=styles['tdl']),
+                               html.Td(telstatus['V20']['altstr'], style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('Az', style=styles['tdr']),
+                               html.Td(telstatus['V5']['azstr'], style=styles['tdl']),
+                               html.Td(telstatus['V20']['azstr'], style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('Target RA', style=styles['tdr']),
+                               html.Td(telstatus['V5']['RAstr'], style=styles['tdl']),
+                               html.Td(telstatus['V20']['RAstr'], style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('Target Dec', style=styles['tdr']),
+                               html.Td(telstatus['V5']['Decstr'], style=styles['tdl']),
+                               html.Td(telstatus['V20']['Decstr'], style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td('ACP Data Age', style=styles['tdr']),
+                               html.Td('{:.1f} min'.format(telstatus['V5']['age']), style=styles['tdl']),
+                               html.Td('{:.1f} min'.format(telstatus['V20']['age']), style=styles['tdl']),
+                              ]),
+                      html.Tr([
+                               html.Td(html.Img(src='http://www.fallingstar.com/weather/mlo/latest_bw400.jpg', width=400), colSpan=3, style=styles['tdc']),
+                              ]),
+                     ], style=styles['table'])
 
-    components.append(html.Span('Status:', style=style))
-    if 'status' in telstatus['V20'].keys():
-        components.append(html.Span('{}'.format(telstatus['V20']['status']), style=style))
-    components.append(html.Br())
 
-    components.append(html.Span('Alt:', style=style))
-    if 'alt' in telstatus['V20'].keys():
-        components.append(html.Span('{:.1f} deg'.format(telstatus['V20']['alt']), style=style))
-    components.append(html.Br())
-
-    components.append(html.Span('Az:', style=style))
-    if 'az' in telstatus['V20'].keys():
-        components.append(html.Span('{:.1f} deg'.format(telstatus['V20']['az']), style=style))
-    components.append(html.Br())
-
-    components.append(html.Span('Target RA (Jnow):', style=style))
-    if 'RA' in telstatus['V20'].keys():
-        components.append(html.Span('{}'.format(telstatus['V20']['RA']), style=style))
-    components.append(html.Br())
-
-    components.append(html.Span('Target Dec (Jnow):', style=style))
-    if 'Dec' in telstatus['V20'].keys():
-        components.append(html.Span('{}'.format(telstatus['V20']['Dec']), style=style))
-    components.append(html.Br())
-
-    components.append(html.Span('ACP Data Age:', style=style))
-    if 'date' in telstatus['V20'].keys():
-        age = (dt.utcnow() - telstatus['V20']['date']).total_seconds()/60.
-        components.append(html.Span('{:.1f} min'.format(age, style=style)))
-    components.append(html.Br())
-
-
-    components.append(html.Hr())
-
-
-#         html.Span('Status:', style=style),
-#         html.Br(),
-#         html.Span('Target RA (Jnow):', style=style),
-#         html.Br(),
-#         html.Span('Target Dec (Jnow):', style=style),
-#         html.Br(),
-#         html.Span('ACP Data Age:', style=style),
-#         html.Br(),
-
+    components = [weather_table,
+                  html.Hr(),
+                  telstatus_table,
+                  ]
     return components
 
-# Multiple components can update everytime interval gets fired.
-# @app.callback(Output('live-update-graph', 'figure'),
-#               events=[Event('interval-component', 'interval')])
-# def update_graph_live():
-#     data = retrieve_weather(lookbackdays=1)
-# 
-#     time = [x['date'] for x in data]
-#     temp = [x['temp']*1.8+32. for x in data]
-#     clouds = [x['clouds']*1.8+32. for x in data]
-# 
-# 
-#     # Create the graph with subplots
-#     fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.4)
-#     fig['layout']['margin'] = {
-#         'l': 30, 'r': 10, 'b': 30, 't': 10
-#     }
-#     fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
-#     fig['layout']['yaxis'] = {'range': [25, 95]}
-# 
-#     fig.append_trace({
-#         'x': time,
-#         'y': temp,
-#         'name': 'Temperature',
-#         'mode': 'lines+markers',
-#         'type': 'scatter'
-#     }, 1, 1)
-# 
-# 
-#     fig['layout']['yaxis'] = {'range': [25, 95]}
-#     fig.append_trace({
-#         'x': time,
-#         'y': clouds,
-#         'name': 'Cloudiness',
-#         'mode': 'lines+markers',
-#         'type': 'scatter'
-#     }, 2, 1)
-# 
-#     return fig
 
 
 if __name__ == '__main__':
