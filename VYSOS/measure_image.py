91f6d2c4d0d017a072bd058b162492ff718e567f
Josh Walawender <jmwalawender@gmail.com>
2018-04-01 12:21:08 -1000

    Updates to measure image

--- VYSOS/measure_image.py
+++ VYSOS/measure_image.py
@@ -10,6 +10,8 @@ import os
 from argparse import ArgumentParser
 import re
 from datetime import datetime as dt
+import tempfile
+
 import mongoengine as me
 
 import numpy as np
@@ -22,25 +24,28 @@ import SIDRE
 
 from VYSOS.schema import Image as ImageDoc
 
+
 ##-------------------------------------------------------------------------
 ## Measure Image
 ##-------------------------------------------------------------------------
 def measure_image(file,\
                  verbose=False,\
                  nographics=False,\
-                 record=False,\
+                 record=True,\
                  ):
+    tick = dt.utcnow()
+    file = os.path.abspath(os.path.expanduser(file))
 
-    file = os.path.expanduser(file)
-    file = os.path.abspath(file)
     ## Try to determine which telescope
     image_info = ImageDoc(filename=os.path.basename(file))
     try:
         image_info.telescope = re.match('(V[25]0?)_.+', image_info.filename).group(1)
     except:
         pass
+    ## Is the image compressed?
     image_info.compressed = (os.path.splitext(image_info.filename)[1] == '.fz')
 
+    ## Set up logfile name and location
     logfilename = os.path.basename(file).replace(".fts", ".log")
     finddate = re.search('(\d{8})at(\d{6})', logfilename)
     if finddate is not None:
@@ -48,7 +53,9 @@ def measure_image(file,\
     if not os.path.exists(os.path.join('/Users/vysosuser/V20Data/AnalysisLogs', imageUTdate)):
         os.mkdir(os.path.join('/Users/vysosuser/V20Data/AnalysisLogs', imageUTdate))
     logfile = os.path.join('/Users/vysosuser/V20Data/AnalysisLogs', imageUTdate, logfilename)
-    im = SIDRE.ScienceImage(file, logfile=logfile, verbose=False)
+
+    im = SIDRE.ScienceImage(file, logfile=logfile, verbose=verbose)
+
     # Exposure Time
     try:
         image_info.exptime = float(im.ccd.header.get('EXPTIME'))
@@ -69,8 +76,6 @@ def measure_image(file,\
         except:
             pass
 
-    print(image_info)
-
     try:
         if not SIDRE.utils.get_master(im.date, type='Bias'):
             SIDRE.calibration.make_master_bias(im.date)
@@ -81,7 +86,7 @@ def measure_image(file,\
     im.create_deviation()
     im.make_source_mask()
     im.subtract_background()
-    wcs = im.solve_astrometry(downsample=2, SIPorder=4)
+    im.solve_astrometry(downsample=2, SIPorder=4)
     perr = im.calculate_pointing_error()
     try:
         image_info.perr_arcmin=perr.to(u.arcmin).value
@@ -96,37 +101,48 @@ def measure_image(file,\
         pass
 
 
+    ## Determine Typical FWHM
+    im.extract()
+    im.determine_FWHM()
+    image_info.FWHM_pix = im.FWHM_pix
+    image_info.ellipticity = im.ellipticity
+
 
     ## Load (local) photometric reference catalog
-    image_info.target = im.ccd.header.get('OBJECT')
-    vprc_path = '/Users/jwalawender/Dropbox/SIDREtest/'
-    vprc_file = os.path.join(vprc_path, 'VPRC_{}.fit'.format(image_info.target))
-    if os.path.exists(vprc_file):
-        im.log.info('Reading VPRC catalog file')
-        cathdul = fits.open(vprc_file, 'readonly')
-        vprc = Table(cathdul[1].data)
-        vprc.add_column(Column(vprc['raMean'], name='RA'))
-        vprc.add_column(Column(vprc['decMean'], name='DEC'))
-        vprc = vprc[vprc['rMeanPSFMag'] != -999.] # Remove entries with invalid r magnitude
-        vprc = vprc[vprc['rMeanPSFMagErr'] != -999.] # Remove entries with invalid r magnitude error
-        vprc = vprc[vprc['rMeanPSFMagErr'] < 0.1] # Remove entries with r magnitude error > 0.1
-        vprc = vprc[vprc['rMeanPSFMag'] < 16.0] # Remove entries r magnitude > 16
-        vprc = vprc[vprc['nDetections'] >= 6] # Remove entried with fewer than 6 detections
-        vprc.keep_columns(['objID', 'RA', 'DEC', 'rMeanPSFMag', 'rMeanPSFMagErr', 'nDetections'])
-        coords = c.SkyCoord(vprc['RA'], vprc['DEC'], unit=u.deg)
+#     image_info.target = im.ccd.header.get('OBJECT')
+#     vprc_path = '/Users/jwalawender/Dropbox/SIDREtest/'
+#     vprc_file = os.path.join(vprc_path, 'VPRC_{}.fit'.format(image_info.target))
+#     if os.path.exists(vprc_file):
+#         im.log.info('Reading VPRC catalog file')
+#         cathdul = fits.open(vprc_file, 'readonly')
+#         vprc = Table(cathdul[1].data)
+#         vprc.add_column(Column(vprc['raMean'], name='RA'))
+#         vprc.add_column(Column(vprc['decMean'], name='DEC'))
+#         vprc = vprc[vprc['rMeanPSFMag'] != -999.] # Remove entries with invalid r magnitude
+#         vprc = vprc[vprc['rMeanPSFMagErr'] != -999.] # Remove entries with invalid r magnitude error
+#         vprc = vprc[vprc['rMeanPSFMagErr'] < 0.1] # Remove entries with r magnitude error > 0.1
+#         vprc = vprc[vprc['rMeanPSFMag'] < 16.0] # Remove entries r magnitude > 16
+#         vprc = vprc[vprc['nDetections'] >= 6] # Remove entried with fewer than 6 detections
+#         vprc.keep_columns(['objID', 'RA', 'DEC', 'rMeanPSFMag', 'rMeanPSFMagErr', 'nDetections'])
+#         coords = c.SkyCoord(vprc['RA'], vprc['DEC'], unit=u.deg)
+# 
+#         im.extract()
+#         im.associate(vprc, magkey='rMeanPSFMag')
+#         im.calculate_zero_point(plot='ZP_PanSTARRS.png')
 
-        im.extract()
-        im.associate(vprc, magkey='rMeanPSFMag')
-        im.calculate_zero_point(plot='ZP_PanSTARRS.png')
+
 
     image_info.analyzed=True
     image_info.SIDREversion=SIDRE.version.version
 
-    if not nographics:
-        jpegfilename='test_PS.jpg'
-        im.render_jpeg(jpegfilename=jpegfilename,
+    if nographics is True:
+        fulljpeg = None
+        cropjpeg = None
+    else:
+        fulljpeg = 'test_PS.jpg'
+        cropjpeg = None
+        im.render_jpeg(jpegfilename=fulljpeg,
                        overplot_assoc=False, overplot_pointing=True)
-        # image_info.full_field_jpeg = open(jpegfilename, 'rb')
     
     if record:
         im.log.info('Connecting to mongo db at 192.168.1.101')
@@ -136,9 +152,37 @@ def measure_image(file,\
             im.log.error('Could not connect to mongo db')
             raise Error('Failed to connect to mongo')
         else:
+            # Remove old entries for this image file
+            for entry in ImageDoc.objects(filename=os.path.basename(file)):
+                im.log.debug('Removing old image info entry for this file')
+                entry.delete()
+            # Save JPEG to MongoDB
+            if not nographics:
+                if fulljpeg is not None:
+                    with open(fulljpeg, 'rb') as imageData:
+                        image_bytes = imageData.read()
+                        with tempfile.TemporaryFile() as f:
+                            f.write(bytearray(image_bytes))
+                            f.flush()
+                            f.seek(0)
+                            image_info.full_field_jpeg.put(f)
+                if cropjpeg is not None:
+                    with open(cropjpeg, 'rb') as imageData:
+                        image_bytes = imageData.read()
+                        with tempfile.TemporaryFile() as f:
+                            f.write(bytearray(image_bytes))
+                            f.flush()
+                            f.seek(0)
+                            image_info.cropped_jpeg.put(f)
+            # Save new entry for this image file
+            im.log.debug('Adding image info to mongo database')
             image_info.save()
     else:
         print(image_info)
+
+    tock = dt.utcnow()
+    elapsed = (tock-tick).total_seconds()
+    im.log.info(f'Processing time = {elapsed:.1f} s')
 
 
 def main():
@@ -150,7 +194,10 @@ def main():
     parser.add_argument("-v", "--verbose",
         action="store_true", dest="verbose",
         default=False, help="Be verbose! (default = False)")
-    parser.add_argument("--no-graphics",
+    parser.add_argument("-p", "--print",
+        action="store_true", dest="printonly",
+        default=False, help="Print results only, do not record to database.")
+    parser.add_argument("-n", "--no-graphics",
         action="store_true", dest="nographics",
         default=False, help="Turn off generation of graphics")
     ## add arguments
@@ -159,8 +206,9 @@ def main():
         help="File Name of Input Image File")
     args = parser.parse_args()
 
-    measure_image(args.filename,\
-                  nographics=args.nographics,\
+    measure_image(args.filename,
+                  nographics=args.nographics,
+                  record=not args.printonly,
                   verbose=args.verbose)
 
 
