46df1f3bba5a4f92c6340ac5048fdf8b7ee616d7
Josh Walawender <jmwalawender@gmail.com>
2018-04-12 12:17:38 -1000

    Refactor to make more easily callable from loop

--- VYSOS/measure_night.py
+++ VYSOS/measure_night.py
@@ -15,7 +15,40 @@ import IQMon
 from measure_image import measure_image
 
 
-def main(argv=None):
+def measure_night(date=None, telescope=None):
+    ##-------------------------------------------------------------------------
+    ## Set date to tonight if not specified
+    ##-------------------------------------------------------------------------
+    now = dt.utcnow()
+    if date is None:
+        date = now.strftime("%Y%m%dUT")
+    
+    ## Set Path to Data for this night
+    paths = [
+             os.path.join(os.path.expanduser("~"), f"{telescope}Data", "Images", date),
+             os.path.join('/', 'Volumes', 'MLOData', telescope, 'Images', date[:4], date),
+             os.path.join('/', 'Volumes', 'DataCopy', telescope, 'Images', date[:4], date),
+            ]
+
+    location = None
+    for path in paths:
+        if os.path.exists(path):
+            print('Found data folder at: {}'.format(path))
+            location = path
+    if location is None:
+        print('Could not find data path for {}'.format(telescope))
+    else:
+        print("Analyzing data for night of "+date)
+        print("Found data at: {}".format(location))
+    
+        files = glob.glob(os.path.join(location, '*.fts'))
+        files.extend(glob.glob(os.path.join(location, '*.fts.fz')))
+        print(f"Found {len(files):d} files in images directory")
+        for file in files:
+            measure_image(file, nographics=True)
+
+
+if __name__ == "__main__":
     ##-------------------------------------------------------------------------
     ## Parse Command Line Arguments
     ##-------------------------------------------------------------------------
@@ -34,40 +67,6 @@ def main(argv=None):
         dest="date", required=False, default="", type=str,
         help="UT date of night to analyze. (i.e. '20130805UT')")
     args = parser.parse_args()
-    
-    
-    ##-------------------------------------------------------------------------
-    ## Set date to tonight if not specified
-    ##-------------------------------------------------------------------------
-    now = dt.utcnow()
-    if args.date is None:
-        args.date = now.strftime("%Y%m%dUT")
-    
-    ## Set Path to Data for this night
-    paths = [
-             os.path.join(os.path.expanduser("~"), f"{args.telescope}Data", "Images", args.date),
-             os.path.join('/', 'Volumes', 'MLOData', args.telescope, 'Images', args.date[:4], args.date),
-             os.path.join('/', 'Volumes', 'DataCopy', args.telescope, 'Images', args.date[:4], args.date),
-            ]
 
-    location = None
-    for path in paths:
-        if os.path.exists(path):
-            print('Found data folder at: {}'.format(path))
-            location = path
-    if location is None:
-        print('Could not find data path for {}'.format(args.telescope))
-    else:
-        print("Analyzing data for night of "+args.date)
-        print("Found data at: {}".format(location))
-    
-        files = glob.glob(os.path.join(location, '*.fts'))
-        files.extend(glob.glob(os.path.join(location, '*.fts.fz')))
-        print(f"Found {len(files):d} files in images directory")
-        for file in files:
-            measure_image(file, nographics=True)
-
-
-if __name__ == "__main__":
-    main()
+    measure_night(date=args.date, telescope=args.telescope)
 
