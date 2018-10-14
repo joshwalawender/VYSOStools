8164ea1f4779f1be9ab860f8add9e5b6d6b2508a
Josh Walawender <jmwalawender@gmail.com>
2018-10-14 09:44:06 -1000

    Draft version to query PANOPTES

--- VYSOS/query_vysos_weather.py
+++ VYSOS/query_vysos_weather.py
@@ -2,11 +2,9 @@ import sys
 import logging
 import argparse
 from datetime import datetime as dt
-from datetime import timedelta as tdelta
 from time import sleep
 import pymongo
-import numpy as np
-# import requests
+import requests
 
 # import mongoengine as me
 # from VYSOS.schema import weather, currentweather
@@ -15,60 +13,37 @@ import numpy as np
 ## Query AAG Solo for Weather Data
 ##-------------------------------------------------------------------------
 def get_weather(logger, robust=True):
-    logger.info('Getting Weather status from PANOPTES')
+    logger.info('Getting Weather status')
+    
+    # http://aagsolo/cgi-bin/cgiLastData
+    # http://aagsolo/cgi-bin/cgiHistData
+    querydate = dt.utcnow()
+    address = 'http://192.168.1.105/cgi-bin/cgiLastData'
 
-    now = dt.utcnow()
-    end = now + tdelta(0,900)
-    start = end - tdelta(0,3600)
+    try:
+        r = requests.get(address)
+    except:
+        logger.error('Failed to connect to AAG Solo')
+    else:
+        lines = r.text.splitlines()
+        result = {}
+        for line in lines:
+            key, val = line.split('=')
+            result[str(key)] = str(val)
+            logger.debug('  {} = {}'.format(key, val))
+        logger.info('  Done.')
 
-    pan001_client = pymongo.MongoClient('192.168.1.50', 27017)
-    pan001_db = pan001_client.panoptes
-    pan001_weather = pan001_db.weather
-    pan001_data = [x for x in pan001_weather.find({'date': {'$gt': start, '$lt': end}},
-                                    sort=[('date', pymongo.DESCENDING)])]
-    latest = pan001_data[0]
-
-    weatherdoc = {"date": latest['date'],
-                  "querydate": now,
-                  "clouds": float(latest['sky_temp_C']),
-                  "temp": float(latest['ambient_temp_C']),
-                  "wind": float(latest['wind_speed_KPH']),
-                  "gust": float(latest['wind_speed_KPH']),
-                  "rain": int(latest['rain_frequency']),
-                  "light": 0,
-                  "switch": {'1': True, '0': False}[latest['safe']],
-                  "safe": {'1': True, '0': False}[latest['safe']],
-                 }
-
-#     http://aagsolo/cgi-bin/cgiLastData
-#     http://aagsolo/cgi-bin/cgiHistData
-#     querydate = dt.utcnow()
-#     address = 'http://192.168.1.105/cgi-bin/cgiLastData'
-# 
-#     try:
-#         r = requests.get(address)
-#     except:
-#         logger.error('Failed to connect to AAG Solo')
-#     else:
-#         lines = r.text.splitlines()
-#         result = {}
-#         for line in lines:
-#             key, val = line.split('=')
-#             result[str(key)] = str(val)
-#             logger.debug('  {} = {}'.format(key, val))
-#         logger.info('  Done.')
-# 
-#         weatherdoc = {"date": dt.strptime(result['dataGMTTime'], '%Y/%m/%d %H:%M:%S'),
-#                       "querydate": querydate,
-#                       "clouds": float(result['clouds']),
-#                       "temp": float(result['temp']),
-#                       "wind": float(result['wind']),
-#                       "gust": float(result['gust']),
-#                       "rain": int(result['rain']),
-#                       "light": int(result['light']),
-#                       "switch": int(result['switch']),
-#                       "safe": {'1': True, '0': False}[result['safe']],
-#                      }
+        weatherdoc = {"date": dt.strptime(result['dataGMTTime'], '%Y/%m/%d %H:%M:%S'),
+                      "querydate": querydate,
+                      "clouds": float(result['clouds']),
+                      "temp": float(result['temp']),
+                      "wind": float(result['wind']),
+                      "gust": float(result['gust']),
+                      "rain": int(result['rain']),
+                      "light": int(result['light']),
+                      "switch": int(result['switch']),
+                      "safe": {'1': True, '0': False}[result['safe']],
+                     }
 
         threshold = 30
         age = (weatherdoc["querydate"] - weatherdoc["date"]).total_seconds()
