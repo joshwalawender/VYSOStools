557ebae5c67fa9ac5a7b16ac64da4c627c7f3804
Josh Walawender <jmwalawender@gmail.com>
2019-01-01 09:13:14 -1000

    Add log messages

--- VYSOS/get_status.py
+++ VYSOS/get_status.py
@@ -196,6 +196,9 @@ def control_by_web(status, logger):
     status['dome_temperature'] = temp1
     status['fan_state'] = r1state
     status['fan_enable'] = r2state
+    logger.info(f'  Dome Temperature = {.1f}')
+    logger.info(f'  Fan State = {r1state}')
+    logger.info(f'  Fan Enable = {r2state}')
 
     return status
 
