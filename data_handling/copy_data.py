b6eb618e5dccb431e032c5c8d11e5b174d7efc0d
Josh Walawender <jmwalawender@gmail.com>
2019-01-06 10:25:35 -1000

    New data handling process

--- data_handling/copy_data.py
+++ data_handling/copy_data.py
@@ -14,17 +14,20 @@ import shutil
 import subprocess
 import re
 
+import numpy as np
 from astropy.io import fits
+
+
 
 ##-------------------------------------------------------------------------
 ## Copy Data
 ##-------------------------------------------------------------------------
-def copy_data(date, telescope, verbose=False):
+def copy_data(date, tel, verbose=False, run=True):
 
     ##-------------------------------------------------------------------------
     ## Create logger object
     ##-------------------------------------------------------------------------
-    logger = logging.getLogger('DataHandler_{}_{}'.format(telescope, date))
+    logger = logging.getLogger('DataHandler_{}_{}'.format(tel, date))
     logger.setLevel(logging.DEBUG)
     ## Set up console output
     LogConsoleHandler = logging.StreamHandler()
@@ -36,7 +39,7 @@ def copy_data(date, telescope, verbose=False):
     LogConsoleHandler.setFormatter(LogFormat)
     logger.addHandler(LogConsoleHandler)
     ## Set up file output
-    LogFileName = 'DataHandler_{}_{}.log'.format(telescope, date)
+    LogFileName = 'DataHandler_{}_{}.log'.format(tel, date)
     LogFilePath = join('/', 'Users', 'vysosuser', 'logs')
     LogFileHandler = logging.FileHandler(join(LogFilePath, LogFileName))
     LogFileHandler.setLevel(logging.DEBUG)
@@ -44,188 +47,91 @@ def copy_data(date, telescope, verbose=False):
     logger.addHandler(LogFileHandler)
 
 
-
     ##-------------------------------------------------------------------------
     ## Copy Data from Source to Destination and make second copy
     ##-------------------------------------------------------------------------
-    source_path = join(expanduser('~vysosuser'), f"{telescope}Data")
+    subdirs = ['Images', 'Logs']
+    for subdir in subdirs:
+        path = None
+        files = []
+        source_path = join(expanduser('~vysosuser'), f"{tel}Data", subdir, date)
+        logger.info(f"Checking for files in {source_path}")
+        dest_paths = [join('/', 'Volumes', 'MLOData', tel, subdir, date[0:4], date),
+                      join('/', 'Volumes', 'DataCopy', tel, subdir, date[0:4], date)]
 
-    dest_path = join('/', 'Volumes', 'MLOData', telescope)
-    assert exists(join(dest_path, 'Images'))
-    assert exists(join(dest_path, 'Logs'))
-    if not exists(join(dest_path, 'Images', date[0:4])):
-        logger.info(f"Making directory: {join(dest_path, 'Images', date[0:4])}")
-        os.mkdir(join(dest_path, 'Images', date[0:4]))
-    if not exists(join(dest_path, 'Logs', date[0:4])):
-        logger.info(f"Making directory: {join(dest_path, 'Logs', date[0:4])}")
-        os.mkdir(join(dest_path, 'Logs', date[0:4]))
-    dest_path_images = join(dest_path, 'Images', date[0:4], date)
-    dest_path_logs = join(dest_path, 'Logs', date[0:4], date)
+        nfiles = 0
+        ndirs = 0
+        for entry in os.walk(source_path):
+            path = entry[0]
+            files = entry[2]
+            logger.info(f"  Found {len(files)} in {path}")
+            ndirs += 1
+            nfiles += len(files)
+        logger.info(f"Found a total of {nfiles} files in {ndirs} directories")
 
-    copy_path = join('/', 'Volumes', 'DataCopy', telescope)
-    assert exists(join(copy_path, 'Images'))
-    assert exists(join(copy_path, 'Logs'))
-    if not exists(join(copy_path, 'Images', date[0:4])):
-        logger.info(f"Making directory: {join(copy_path, 'Images', date[0:4])}")
-        os.mkdir(join(copy_path, 'Images', date[0:4]))
-    if not exists(join(copy_path, 'Logs', date[0:4])):
-        logger.info(f"Making directory: {join(copy_path, 'Logs', date[0:4])}")
-        os.mkdir(join(copy_path, 'Logs', date[0:4]))
-    copy_path_images = join(copy_path, 'Images', date[0:4], date)
-    copy_path_logs = join(copy_path, 'Logs', date[0:4], date)
+        filecount = 0
+        for entry in os.walk(source_path):
+            path = entry[0]
+            files = entry[2]
+            logger.info(f"  Handling {len(files)} in {path}")
+            for file in files:
+                filecount += 1
+                logger.info(f"Checking source {filecount} of {nfiles}: {join(path, file)}")
+                ok2del = [False]*len(dest_paths)
+                for i,dest_path in enumerate(dest_paths):
+                    destination = path.replace(source_path, dest_path)
+                    dest_file = join(destination, file)
+                    logger.info(f"  Checking Destination: {destination}")
+                    if not os.path.exists(destination):
+                        if run: os.mkdir(destination)
+                    if run: assert os.path.exists(destination)
+                    fileext = os.path.splitext(file)[1]
+                    
+                    # Handle FITS files
+                    if fileext in ['.fts', '.fits']:
+                        source_fz = f'{file}.fz'
+                        dest_fz = f'{dest_file}.fz'
 
-    ## Check that date directory has been made on Destination
-    if not exists(dest_path_images):
-        logger.info(f"Making directory: {dest_path_images}")
-        os.mkdir(dest_path_images)
-    if not exists(dest_path_logs):
-        logger.info(f"Making directory: {dest_path_logs}")
-        os.mkdir(dest_path_logs)
 
-    ## Check that date directory has been made on copy
-    if not exists(copy_path_images):
-        logger.info(f"Making directory: {copy_path_images}")
-        os.mkdir(copy_path_images)
-    if not exists(copy_path_logs):
-        logger.info(f"Making directory: {copy_path_logs}")
-        os.mkdir(copy_path_logs)
-
-    ## Make list of files to analyze
-    files = glob.glob(join(source_path, 'Images', date, '*.*'))
-    if exists(join(source_path, 'Images', date, 'Calibration')):
-        files.extend(glob.glob(join(source_path, 'Images', date, 'Calibration', '*.*')))
-        if not exists(join(dest_path_images, 'Calibration')):
-            os.mkdir(join(dest_path_images, 'Calibration'))
-        if not exists(join(copy_path_images, 'Calibration')):
-            os.mkdir(join(copy_path_images, 'Calibration'))
-    if exists(join(source_path, 'Images', date, 'AutoFlat')):
-        files.extend(glob.glob(join(source_path, 'Images', date, 'AutoFlat', '*.*')))
-        if not exists(join(dest_path_images, 'AutoFlat')):
-            os.mkdir(join(dest_path_images, 'AutoFlat'))
-        if not exists(join(copy_path_images, 'AutoFlat')):
-            os.mkdir(join(copy_path_images, 'AutoFlat'))
-    files.extend(glob.glob(join(source_path, 'Logs', date, '*.*')))
-    logger.info('Found {} files to analyze'.format(len(files)))
-
-    # Loop over all files
-    for i,file in enumerate(files):
-        filename = os.path.split(file)[1]
-        fileext = os.path.splitext(filename)[1]
-        logger.info('Checking file {}/{}: {}'.format(i+1, len(files), filename))
-
-        if fileext in ['.fts', '.fz', '.fits']:
-            dest_file = join(dest_path_images, filename)
-            copy_file = join(copy_path_images, filename)
-            logger.debug(f"Destination file: {dest_file}")
-            logger.debug(f"Copy file: {copy_file}")
-
-            dest_fz = '{}.fz'.format(dest_file)
-            copy_fz = '{}.fz'.format(copy_file)
-            logger.debug(f"Destination fz file: {dest_fz}")
-            logger.debug(f"Copy fz file: {copy_fz}")
-
-            to_dest = False
-            if exists(dest_path):
-                if not exists(dest_file) and not exists(dest_fz):
-                    to_dest=True
-                elif exists(dest_file) and not exists(dest_fz):
-                    logger.info('  Compressing existing file on MLOData')
-                    subprocess.call(['fpack', dest_file])
-                    if exists(dest_fz):
-                        os.remove(dest_file)
-
-            to_ext = False
-            if exists(copy_path):
-                if not exists(copy_file) and not exists(copy_fz):
-                    to_ext=True
-                elif exists(copy_file) and not exists(copy_fz):
-                    logger.info('  Compressing existing file on DataCopy')
-                    subprocess.call(['fpack', copy_file])
-                    if exists(copy_fz):
-                        os.remove(copy_file)
-
-            if to_dest or to_ext:
-                with fits.open(file, checksum=True) as hdul:
-                    hdul[0].add_checksum()
-                    if to_dest:
-                        logger.info('  File does not exist on MLOData.  Writing compressed file.')
-                        hdul.writeto(dest_file, checksum=True)
-                        subprocess.call(['fpack', dest_file])
+                        if not exists(dest_file) and not exists(dest_fz):
+                            logger.info(f'    File does not exist on {dest_path}. Writing file.')
+                            if run:
+                                with fits.open(join(path, file), checksum=True) as hdul:
+                                    hdul[0].add_checksum()
+                                    hdul.writeto(dest_file, checksum=True)
+                        if exists(dest_file) and not exists(dest_fz):
+                            logger.info(f'    Compressing file: {dest_file}')
+                            if run:
+                                subprocess.call(['fpack', dest_file])
+                                if exists(dest_fz):
+                                    os.remove(dest_file)
                         if exists(dest_fz):
-                            os.remove(dest_file)
-                    if to_ext:
-                        logger.info('  File does not exist on DataCopy.  Writing compressed file.')
-                        hdul.writeto(copy_file, checksum=True)
-                        subprocess.call(['fpack', copy_file])
-                        if exists(copy_fz):
-                            os.remove(copy_file)
-            # Verify checksums on drobo
-            failcheck_drobo = bool(subprocess.call(['fitscheck', dest_fz]))
-            if failcheck_drobo:
-                logger.warning('Checksum failed on {}'.format(dest_fz))
-            # Verify checksums on ext
-            failcheck_ext = bool(subprocess.call(['fitscheck', copy_fz]))
-            if failcheck_ext:
-                logger.warning('Checksum failed on {}'.format(copy_fz))
-
-            if not failcheck_drobo and not failcheck_ext:
-                if args.delete:
-                    logger.info('  All CHECKSUM verifications passed.  Deleting file.')
-                    os.remove(file)
+                            logger.info(f'    File {os.path.split(dest_fz)[1]} exists on {dest_path}')
+                            if run:
+                                failcheck = bool(subprocess.call(['fitscheck', dest_fz]))
+                                if failcheck is True:
+                                    logger.error("    Checksum Failed")
+                                else:
+                                    logger.info("    Checksum Passed")
+                                ok2del[i] = not failcheck
+                            else:
+                                ok2del[i] = True
+                    # Handle non-FITS files
+                    else:
+                        if not exists(dest_file):
+                            logger.info(f'    Copying {file} to {destination}')
+                            if run: shutil.copy2(join(path, file), dest_file)
+                        if os.path.exists(dest_file) or not run:
+                            ok2del[i] = True
+                if args.delete and np.all(ok2del):
+                    logger.info(f'  Deleting {file}')
+                    if run: os.remove(join(path, file))
+            if args.delete:
+                if len(glob.glob(join(path, '*'))) == 0:
+                    logger.info('Removing {}'.format(path))
+                    if run: os.rmdir(path)
                 else:
-                    logger.info('  All CHECKSUM verifications passed.')
-
-        ## No checksum verification for non-FITS files
-        elif fileext in ['.txt', '.log']:
-            dest_file = join(dest_path_logs, filename)
-            copy_file = join(copy_path_logs, filename)
-
-            if not exists(dest_file):
-                logger.info('Copying {} to MLOData'.format(file))
-                shutil.copy2(file, dest_file)
-            if not exists(copy_file):
-                logger.info('Copying {} to DataCopy'.format(file))
-                shutil.copy2(file, dest_file)
-            if args.delete and exists(copy_path) and exists(dest_path):
-                logger.info('Deleting {}'.format(file))
-                os.remove(file)
-
-    if args.delete:
-        ## Remove Calibration directory if empty
-        Calibration_path = join(source_path, 'Images', date, 'Calibration')
-        if exists(Calibration_path):
-            if len(glob.glob(join(Calibration_path, '*'))) == 0:
-                logger.info('Removing {}'.format(Calibration_path))
-                os.rmdir(Calibration_path)
-            else:
-                logger.warning('Files still remain in {}'.format(Calibration_path))
-
-        ## Remove AutoFlat directory if empty
-        AutoFlat_path = join(source_path, 'Images', date, 'AutoFlat')
-        if exists(AutoFlat_path):
-            if len(glob.glob(join(AutoFlat_path, '*'))) == 0:
-                logger.info('Removing {}'.format(AutoFlat_path))
-                os.rmdir(AutoFlat_path)
-            else:
-                logger.warning('Files still remain in {}'.format(AutoFlat_path))
-
-        ## Remove Images directory if empty
-        Images_path = join(source_path, 'Images', date)
-        if exists(Images_path):
-            if len(glob.glob(join(Images_path, '*'))) == 0:
-                logger.info('Removing {}'.format(Images_path))
-                os.rmdir(Images_path)
-            else:
-                logger.warning('Files still remain in {}'.format(Images_path))
-
-        ## Remove Logs directory if empty
-        Logs_path = join(source_path, 'Logs', date)
-        if exists(Logs_path):
-            if len(glob.glob(join(Logs_path, '*'))) == 0:
-                logger.info('Removing {}'.format(Logs_path))
-                os.rmdir(Logs_path)
-            else:
-                logger.warning('Files still remain in {}'.format(Logs_path))
+                    logger.warning(f'Files still remain in {path}')
 
 
 if __name__ == '__main__':
