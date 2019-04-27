b8d7ce452671afe3d848f043559cf69ecec1a700
Josh Walawender <jmwalawender@gmail.com>
2019-04-26 17:04:14 -1000

    Revamp generation of RMTL to split up by project

--- scheduling/convert_to_rtml.py
+++ scheduling/convert_to_rtml.py
@@ -8,19 +8,24 @@ from astropy import units as u
 
 def write_request(f, entry, telescope='V5'):
     filter = {'V5': 'PSr', 'V20': 'PSi'}[telescope]
-    if entry['period'] == 1:
+    if entry['comment'] == 'Filler':
+        project = 'Filler Project'
+        bestefforts = 'true'
+    elif entry['period'] == 1:
+        bestefforts = 'false'
         if entry['count'] > 3:
             project = 'Dwell on Targets'
         else:
             project = 'Monitor Every {:d} Days'.format(entry['period'])
     else:
+        bestefforts = 'false'
         project = 'Monitor Every {:d} Days'.format(entry['period'])
     c = SkyCoord('{} {}'.format(entry['ra'], entry['dec']),
                  unit=(u.hourangle, u.deg),
                  frame='icrs',
                  equinox='J2000',
                  )
-    f.write('   <Request bestefforts="false">\n')
+    f.write(f'   <Request bestefforts="{bestefforts}">\n')
     f.write('       <ID>{}</ID>\n'.format(entry['name']))
     f.write(f'       <UserName>VYSOS-{telescope[1:]}</UserName>\n')
     f.write('       <Description>Observing Plan for {}</Description>\n'.format(entry['name']))
@@ -51,30 +56,57 @@ def write_request(f, entry, telescope='V5'):
     f.write('\n')
 
 
+
 def main(telescope='V5'):
     assert telescope in ['V5', 'V20']
     input_filename = f'{telescope}_targets.txt'
-    rtml_file = f'{telescope}_targets.rtml'
     tab = Table.read(input_filename, format='ascii.csv')
-    if os.path.exists(rtml_file):
-        os.remove(rtml_file)
-    with open(rtml_file, 'w') as f:
-        ## Header
-        f.write('\n')
-        f.write('<RTML>\n')
-        f.write('   <Contact>\n')
-        f.write(f'       <User>VYSOS-{telescope[1:]}</User>\n')
-        f.write('       <Email>vysostelescope@gmail.com</Email>\n')
-        f.write('   </Contact>\n')
-        f.write('\n')
-        byperiod = tab.group_by('period')
-        periods = byperiod.groups
-        for entry in byperiod:
-            moncount = np.where(byperiod.groups.keys['period'] == entry['period'])[0][0]
-            entry['priority'] += 10*moncount
-            write_request(f, entry, telescope=telescope)
-        f.write('</RTML>\n')
+    bycomment = tab.group_by('comment')
+    for group in bycomment.groups:
+        # Write Filler Project RTML File
+        if group[0]['comment'] == 'Filler':
+            project = 'Filler'
+            rtml_file = f'{telescope}_{project}.rtml'
+            if os.path.exists(rtml_file):
+                os.remove(rtml_file)
+            with open(rtml_file, 'w') as f:
+                ## Header
+                f.write('\n')
+                f.write('<RTML>\n')
+                f.write('   <Contact>\n')
+                f.write(f'       <User>VYSOS-{telescope[1:]}</User>\n')
+                f.write('       <Email>vysostelescope@gmail.com</Email>\n')
+                f.write('   </Contact>\n')
+                f.write('\n')
+                for entry in group:
+                    write_request(f, entry, telescope=telescope)
+                f.write('</RTML>\n')
 
+        # Write VYSOS Project RTML Files for Each Period
+        elif group[0]['comment'] == 'VYSOS':
+            byperiod = group.group_by('period')
+            for periodgroup in byperiod.groups:
+                period = periodgroup[0]['period']
+                project = f'VYSOS_{period:02d}day'
+                rtml_file = f'{telescope}_{project}.rtml'
+                if os.path.exists(rtml_file):
+                    os.remove(rtml_file)
+                with open(rtml_file, 'w') as f:
+                    ## Header
+                    f.write('\n')
+                    f.write('<RTML>\n')
+                    f.write('   <Contact>\n')
+                    f.write(f'       <User>VYSOS-{telescope[1:]}</User>\n')
+                    f.write('       <Email>vysostelescope@gmail.com</Email>\n')
+                    f.write('   </Contact>\n')
+                    f.write('\n')
+                    for entry in periodgroup:
+                        moncount = np.where(byperiod.groups.keys['period'] == entry['period'])[0][0]
+                        entry['priority'] += 10*moncount
+                        write_request(f, entry, telescope=telescope)
+                    f.write('</RTML>\n')
+        else:
+                pass
 
 
 if __name__ == '__main__':
